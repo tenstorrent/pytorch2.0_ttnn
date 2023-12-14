1163b077883884df449cfb3127f3c0a3902715b3
yoco <yoco@skymizer.com>
2023-12-14 04:33:58 +0000

    Just some refactoring
    - Extrace passes
    - Rename

--- torch_ttnn.py
+++ torch_ttnn.py
@@ -17,6 +17,11 @@ def linear(i, weight, bias):
 def replace_with_ttnn(node, func, device):
     """
     Replace a node with a new node that calls the given function with the same arguments.
+    It will insert data movement and data conversion before and after the call.
+    Replace:
+        node
+    With:
+        from_torch => to_device => op-func => from_device => to_torch
     """
     graph: torch.fx.Graph = node.graph
     with graph.inserting_before(node):
@@ -28,9 +33,9 @@ def replace_with_ttnn(node, func, device):
         node.replace_all_uses_with(to_torch)
         graph.erase_node(node)
 
-def eliminate_paired_data_movement(node, func, pre_func, check_device = False):
+def eliminate_redundant_data_movement_conversion(node, func, pre_func, check_device = False):
     """
-    Eliminate redudent pattern of paired data movement, such as from_device => to_device.
+    Eliminate redundant pattern of paired data movement, such as from_device => to_device.
     """
     changed = False
     available_func_list = [ttnn.from_device, ttnn.to_device, ttnn.from_torch, ttnn.to_torch]
@@ -47,24 +52,20 @@ def eliminate_paired_data_movement(node, func, pre_func, check_device = False):
                 changed = True
     return changed
 
-def eliminate_paired_data_movement_pass(gm: torch.fx.GraphModule):
+def eliminate_redundant_data_movement_conversion_pass(gm: torch.fx.GraphModule):
     changed = False
     for node in gm.graph.nodes:
-        changed |= eliminate_paired_data_movement(node, ttnn.to_device, ttnn.from_device, check_device=True)
-        changed |= eliminate_paired_data_movement(node, ttnn.from_device, ttnn.to_device, check_device=True)
-        changed |= eliminate_paired_data_movement(node, ttnn.to_torch, ttnn.from_torch)
-        changed |= eliminate_paired_data_movement(node, ttnn.from_torch, ttnn.to_torch)
+        changed |= eliminate_redundant_data_movement_conversion(node, ttnn.to_device, ttnn.from_device, check_device=True)
+        changed |= eliminate_redundant_data_movement_conversion(node, ttnn.from_device, ttnn.to_device, check_device=True)
+        changed |= eliminate_redundant_data_movement_conversion(node, ttnn.to_torch, ttnn.from_torch)
+        changed |= eliminate_redundant_data_movement_conversion(node, ttnn.from_torch, ttnn.to_torch)
     if changed:
         gm.graph.eliminate_dead_code()
         gm.recompile()
     return changed
 
-def aten_backend(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor], options={}) -> torch.fx.GraphModule:
-    """
-    The backend for torch.compile that converts a graph to use ttnn.
-    The graph is wrapped in torch._dynamo.backends.common.aot_autograd, which
-    trace into low level ATen ops not only high level torch ops.
-    """
+
+def rewrite_tt_op_pass(gm: torch.fx.GraphModule):
     device = ttnn.Device(device_id)
     graph: torch.fx.Graph = gm.graph
     for node in gm.graph.nodes:
@@ -73,13 +74,24 @@ def aten_backend(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor], o
         elif node.op == 'call_function' and node.target == torch.ops.aten.mul.Tensor:
             replace_with_ttnn(node, ttnn.matmul, device)
 
-    gm.recompile()
 
+def aten_backend(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor], options={}) -> torch.fx.GraphModule:
+    """
+    The backend for torch.compile that converts a graph to use ttnn.
+    The graph is wrapped in torch._dynamo.backends.common.aot_autograd, which
+    trace into low level ATen ops not only high level torch ops.
+    """
+    
+    # Rewrite with ttnn ops, will insert redundant data movement
+    rewrite_tt_op_pass(gm)
+    
+    # After rewrite, remove redundant data movement
     while True:
-        if not eliminate_paired_data_movement_pass(gm):
+        if not eliminate_redundant_data_movement_conversion_pass(gm):
             break
 
     gm.graph.print_tabular()
+    gm.recompile()
     return gm
 
 from torch._dynamo.backends.common import aot_autograd
