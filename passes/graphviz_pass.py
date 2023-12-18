652ec98133931945066cd6c03bcd6354afb8162c
yoco <yoco@skymizer.com>
2023-12-18 14:45:44 +0000

    Use torch PassManager
    - Extract to_tt_pass
    - Extract graphviz_pass
    - Extract eliminate_data_move_pass

---
+++ passes/graphviz_pass.py
@@ -0,0 +1,20 @@
+import torch
+import fx_graphviz
+
+try:
+    import ttnn
+except ImportError:
+    print("ttnn is not installed, use mock_ttnn instead")
+    import mock_ttnn as ttnn
+
+from torch.fx.passes.infra.pass_base import PassBase, PassResult
+
+
+class GraphvizPass(PassBase):
+    def __init__(self, filename):
+        super().__init__()
+        self.filename = filename
+
+    def call(self, gm: torch.fx.GraphModule):
+        fx_graphviz.to_svg(gm.graph, self.filename)
+        return PassResult(gm, False)
