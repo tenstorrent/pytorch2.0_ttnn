7db5e8b180a62f53ef828b6230a9efc61f4e4902
swimdi <swimdi@skymizer.com>
2024-01-25 12:40:39 +0800

    refine stat_pass.py format

--- passes/stat_pass.py
+++ passes/stat_pass.py
@@ -5,22 +5,22 @@ from torch.fx.passes.fake_tensor_prop import FakeTensorProp
 from torch.fx.passes.infra.pass_base import PassBase, PassResult
 
 def parse_fx_stat(gm: torch.fx.GraphModule, example_inputs, out_file):
-        FakeTensorProp(gm).propagate(*example_inputs)
-        out = []
-        for node in gm.graph.nodes:
-            if node.op not in ["call_function", "call_method"]:
-                continue
-            node_info = {}
-            node_info["op_type"] = str(node.target)
-            if 'tensor_meta' in node.meta:
-                output = {"shape": list(node.meta['tensor_meta'].shape),
-                          "dtype": str(node.meta['tensor_meta'].dtype)}
-                node_info["output"] = output
-            out.append(node_info)
-        os.makedirs(os.path.dirname(out_file), exist_ok=True)
+    FakeTensorProp(gm).propagate(*example_inputs)
+    out = []
+    for node in gm.graph.nodes:
+        if node.op not in ["call_function", "call_method"]:
+            continue
+        node_info = {}
+        node_info["op_type"] = str(node.target)
+        if 'tensor_meta' in node.meta:
+            output = {"shape": list(node.meta['tensor_meta'].shape),
+                        "dtype": str(node.meta['tensor_meta'].dtype)}
+            node_info["output"] = output
+        out.append(node_info)
+    os.makedirs(os.path.dirname(out_file), exist_ok=True)
 
-        with open(out_file, "w") as f:
-            json.dump(out, f, indent=4)
+    with open(out_file, "w") as f:
+        json.dump(out, f, indent=4)
 
 # The pass to collect node's information
 # Run tools/generate_report.py to genetate report
