65208fdd556391d4ed3e174a31788b4f8c6fcaf2
yoco <yoco@skymizer.com>
2024-03-15 07:23:18 +0000

    Revert "Use subgraph_rewriter"
    This reverts commit fc09080676b3dbffbbb7d3a26f6af5fba1efe358.

--- torch_ttnn/patterns/add.py
+++ torch_ttnn/patterns/add.py
@@ -7,29 +7,12 @@ except ImportError:
     from .. import mock_ttnn as ttnn
 
 
-# NOTE(yoco) The name `add` must be the same as the name of the function.
-# Because in the implementation of fx.wrap use fn.__name__ to get the
-# name of the function. If we write
-#
-#    some_other_name = ttnn.add
-#
-# The fn.__name__ will be `add` instead of `some_other_name`, because
-# it is just a reference to the function `ttnn.add`.
-#
-# In the implementation of torch, it will use the name of the function
-# to find the original function in current frame's globals and patch it.
-# If the name of the function is `some_other_name`, it will not find the
-# original function and raise an error.
-add = ttnn.add
-torch.fx.wrap(add)
-
-
 def add_pat(x, y):
     return torch.ops.aten.add.Tensor(x, y)
 
 
 def add_rep(x, y):
-    return add(x, y)
+    return ttnn.add(x, y)
 
 
 def add_pat2(x):
@@ -37,7 +20,7 @@ def add_pat2(x):
 
 
 def add_rep2(x):
-    return add(x, x)
+    return ttnn.add(x, x)
 
 
 pat_rep_list = [
