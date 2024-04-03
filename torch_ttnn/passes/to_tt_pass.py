3bfefeeec32ffab13ab52b6bab6f5bfe0826e1a9
yoco <yoco@skymizer.com>
2024-04-03 14:40:05 +0000

    Simplify op conversion

--- torch_ttnn/passes/to_tt_pass.py
+++ torch_ttnn/passes/to_tt_pass.py
@@ -4,8 +4,11 @@ import ttnn
 from torch.fx.passes.infra.pass_base import PassBase, PassResult
 
 
-class ReplacePointwiseUnary(torch.fx.Transformer):
+class ReplaceSimpleOpMap(torch.fx.Transformer):
     opmap = {
+        ############################################################
+        # Pointwise unary
+        ############################################################
         torch.ops.aten.abs.default: ttnn.abs,
         torch.ops.aten.acos.default: ttnn.acos,
         torch.ops.aten.acosh.default: ttnn.acosh,
@@ -41,16 +44,9 @@ class ReplacePointwiseUnary(torch.fx.Transformer):
         torch.ops.aten.sqrt.default: ttnn.sqrt,
         torch.ops.aten.tan.default: ttnn.tan,
         torch.ops.aten.tanh.default: ttnn.tanh,
-    }
-
-    def call_function(self, target, args, kwargs):
-        if target in self.opmap:
-            return super().call_function(self.opmap[target], args, kwargs)
-        return super().call_function(target, args, kwargs)
-
-
-class ReplacePointwiseBinary(torch.fx.Transformer):
-    opmap = {
+        ############################################################
+        # Pointwise binary
+        ############################################################
         torch.ops.aten.add.Tensor: ttnn.add,
         torch.ops.aten.eq.Tensor: ttnn.eq,
         torch.ops.aten.gt.Tensor: ttnn.gt,
@@ -65,32 +61,23 @@ class ReplacePointwiseBinary(torch.fx.Transformer):
         torch.ops.aten.pow.Tensor_Tensor: ttnn.pow,
         torch.ops.aten.sub.Tensor: ttnn.sub,
         torch.ops.aten.xlogy.Tensor: ttnn.xlogy,
-    }
-
-    def call_function(self, target, args, kwargs):
-        if target in self.opmap:
-            return super().call_function(self.opmap[target], args, kwargs)
-        return super().call_function(target, args, kwargs)
-
-
-class ReplacePointwiseTrinary(torch.fx.Transformer):
-    opmap = {
+        ############################################################
+        # Pointwise trinary
+        ############################################################
         torch.ops.aten.addcdiv.default: ttnn.addcdiv,
         torch.ops.aten.addcmul.default: ttnn.addcmul,
         # torch.ops.aten.mac.Tensor: ttnn.mac,        # NOTE(yoco): I don't what ttnn.mac is
         torch.ops.aten.where.self: ttnn.where,
-    }
-
-    def call_function(self, target, args, kwargs):
-        if target in self.opmap:
-            return super().call_function(self.opmap[target], args, kwargs)
-        return super().call_function(target, args, kwargs)
-
-
-class ReplaceMatrixMultiplication(torch.fx.Transformer):
-    opmap = {
+        ############################################################
+        # Matrix multiplication
+        ############################################################
         torch.ops.aten.mm.default: ttnn.matmul,
         torch.ops.aten.linear.default: ttnn.linear,
+        ############################################################
+        # Data movement
+        ############################################################
+        torch.ops.aten.view.default: ttnn.reshape,
+        torch.ops.aten.permute.default: ttnn.permute,
     }
 
     def call_function(self, target, args, kwargs):
@@ -101,27 +88,18 @@ class ReplaceMatrixMultiplication(torch.fx.Transformer):
 
 class ReplaceMoreTt(torch.fx.Transformer):
     def call_function(self, target, args, kwargs):
-        if target == torch.ops.aten.mm.default:
-            return super().call_function(ttnn.matmul, args, kwargs)
-        elif target == torch.ops.aten._softmax.default:
+        if target == torch.ops.aten._softmax.default:
             return super().call_function(ttnn.softmax, args[:2], kwargs)
-        elif target == torch.ops.aten.view.default:
-            return super().call_function(ttnn.reshape, args, kwargs)
-        elif target == torch.ops.aten.permute.default:
-            return super().call_function(ttnn.permute, args, kwargs)
         return super().call_function(target, args, kwargs)
 
 
 class ToTtPass(PassBase):
     def call(self, gm: torch.fx.GraphModule):
-        # NOTE(yoco): In our case, subgraph_rewriter actually
-        # is not a best choice. We should use torch.fx.Transformer.
-        # However, we use subgraph_rewriter for demonstration
-        # and as a code stub. Because Transformer only support
-        # 1-to-N replacement. For N-to-M replacement, we need
-        # to use subgraph_rewriter.
-        # import patterns.add
+        # NOTE(yoco):
+        # 1-to-N: use Transformer
+        # N-to-M: use subgraph_rewriter.
 
+        # Transformer
         pat_rep_list = list()
         # Patterns and replacements
         from ..patterns import linear
@@ -135,10 +113,7 @@ class ToTtPass(PassBase):
             modified = modified or len(replaced_pats) > 0
 
         # Replace more patterns with torch.fx.Transformer
+        gm = ReplaceSimpleOpMap(gm).transform()
         gm = ReplaceMoreTt(gm).transform()
-        gm = ReplacePointwiseUnary(gm).transform()
-        gm = ReplacePointwiseBinary(gm).transform()
-        gm = ReplacePointwiseTrinary(gm).transform()
-        gm = ReplaceMatrixMultiplication(gm).transform()
 
         return PassResult(gm, True)
