138e67edec37dc9ba265353781866778c0ad183f
yoco <yoco@skymizer.com>
2024-04-08 10:24:15 +0000

    Fix more ops unit test

--- tests/test_more_ops.py
+++ tests/test_more_ops.py
@@ -4,28 +4,6 @@ import ttnn
 import unittest
 
 
-class SubModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x, y):
-        return x - y
-
-    def input_shapes(self):
-        return [(4, 4), (4, 4)]
-
-
-class MulModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x, y):
-        return torch.mul(x, y)
-
-    def input_shapes(self):
-        return [(4, 4), (4, 4)]
-
-
 class SoftmaxModule(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -37,17 +15,6 @@ class SoftmaxModule(torch.nn.Module):
         return [(1, 1, 64, 32)]
 
 
-class TanhModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x):
-        return torch.tanh(x)
-
-    def input_shapes(self):
-        return [(4, 4)]
-
-
 class ReshapeModule(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -56,7 +23,7 @@ class ReshapeModule(torch.nn.Module):
         return torch.reshape(x, new_shape)
 
     def input_shapes(self):
-        return [(4, 4)]
+        return [(32, 64)]
 
 
 class PermuteModule(torch.nn.Module):
@@ -79,54 +46,6 @@ class TestModules(unittest.TestCase):
         # Close the device
         ttnn.close_device(self.device)
 
-    def test_sub(self):
-        m = SubModule()
-        input_shapes = m.input_shapes()
-        inputs = [torch.rand(shape, dtype=torch.bfloat16) for shape in input_shapes]
-        result_before = m.forward(*inputs)
-        option = torch_ttnn.TenstorrentBackendOption(device=self.device)
-        option.gen_graphviz = True
-        # The compilation is lazy, so we need to run forward once to trigger the compilation
-        m = torch.compile(m, backend="ttnn", options=option)
-        result_after = m.forward(*inputs)
-        option._out_fx_graphs[0].print_tabular()
-
-        # Check the graph has be rewritten and contain ttnn ops
-        nodes = list(option._out_fx_graphs[0].nodes)
-        self.assertTrue(nodes[6].target == ttnn.sub)
-        self.assertTrue(nodes[6].args[0].target == ttnn.to_device)
-        self.assertTrue(nodes[6].args[0].args[0].target == ttnn.from_torch)
-        self.assertTrue(nodes[7].target == ttnn.from_device)
-        self.assertTrue(nodes[8].target == ttnn.to_layout)
-        self.assertTrue(nodes[9].target == ttnn.to_torch)
-        # Check inference result
-        self.assertTrue(torch.allclose(result_before, result_after))
-
-    def test_mul(self):
-        m = MulModule()
-        input_shapes = m.input_shapes()
-        inputs = [
-            torch.randint(1, 5, shape).type(torch.bfloat16) for shape in input_shapes
-        ]
-        result_before = m.forward(*inputs)
-        option = torch_ttnn.TenstorrentBackendOption(device=self.device)
-        option.gen_graphviz = True
-        # The compilation is lazy, so we need to run forward once to trigger the compilation
-        m = torch.compile(m, backend="ttnn", options=option)
-        result_after = m.forward(*inputs)
-        option._out_fx_graphs[0].print_tabular()
-
-        # Check the graph has be rewritten and contain ttnn ops
-        nodes = list(option._out_fx_graphs[0].nodes)
-        self.assertTrue(nodes[6].target == ttnn.mul)
-        self.assertTrue(nodes[6].args[0].target == ttnn.to_device)
-        self.assertTrue(nodes[6].args[0].args[0].target == ttnn.from_torch)
-        self.assertTrue(nodes[7].target == ttnn.from_device)
-        self.assertTrue(nodes[8].target == ttnn.to_layout)
-        self.assertTrue(nodes[9].target == ttnn.to_torch)
-        # Check inference result
-        self.assertTrue(torch.allclose(result_before, result_after))
-
     def test_softmax(self):
         m = SoftmaxModule()
         input_shapes = m.input_shapes()
@@ -142,35 +61,13 @@ class TestModules(unittest.TestCase):
 
         # Check the graph has be rewritten and contain ttnn ops
         nodes = list(option._out_fx_graphs[0].nodes)
-        self.assertTrue(nodes[3].target == ttnn.softmax)
-        self.assertTrue(nodes[3].args[0].target == ttnn.to_device)
-        self.assertTrue(nodes[3].args[0].args[0].target == ttnn.from_torch)
-        self.assertTrue(nodes[4].target == ttnn.from_device)
-        self.assertTrue(nodes[5].target == ttnn.to_layout)
-        self.assertTrue(nodes[6].target == ttnn.to_torch)
-        # Check inference result
-        self.assertTrue(torch.allclose(result_before, result_after, rtol=0.2))
-
-    def test_tanh(self):
-        m = TanhModule()
-        input_shapes = m.input_shapes()
-        inputs = [torch.rand(shape, dtype=torch.bfloat16) for shape in input_shapes]
-        result_before = m.forward(*inputs)
-        option = torch_ttnn.TenstorrentBackendOption(device=self.device)
-        option.gen_graphviz = True
-        # The compilation is lazy, so we need to run forward once to trigger the compilation
-        m = torch.compile(m, backend="ttnn", options=option)
-        result_after = m.forward(*inputs)
-        option._out_fx_graphs[0].print_tabular()
-
-        # Check the graph has be rewritten and contain ttnn ops
-        nodes = list(option._out_fx_graphs[0].nodes)
-        self.assertTrue(nodes[3].target == ttnn.tanh)
-        self.assertTrue(nodes[3].args[0].target == ttnn.to_device)
-        self.assertTrue(nodes[3].args[0].args[0].target == ttnn.from_torch)
-        self.assertTrue(nodes[4].target == ttnn.from_device)
-        self.assertTrue(nodes[5].target == ttnn.to_layout)
-        self.assertTrue(nodes[6].target == ttnn.to_torch)
+        self.assertTrue(nodes[1].target == ttnn.from_torch)
+        self.assertTrue(nodes[2].target == ttnn.to_layout)
+        self.assertTrue(nodes[3].target == ttnn.to_device)
+        self.assertTrue(nodes[4].target == ttnn.softmax)
+        self.assertTrue(nodes[5].target == ttnn.from_device)
+        self.assertTrue(nodes[6].target == ttnn.to_layout)
+        self.assertTrue(nodes[7].target == ttnn.to_torch)
         # Check inference result
         self.assertTrue(torch.allclose(result_before, result_after, rtol=0.2))
 
@@ -178,7 +75,7 @@ class TestModules(unittest.TestCase):
         m = ReshapeModule()
         input_shapes = m.input_shapes()
         inputs = [torch.rand(shape, dtype=torch.bfloat16) for shape in input_shapes]
-        new_shape = (2, 8)
+        new_shape = (64, 32)
         result_before = m.forward(*inputs, new_shape)
         option = torch_ttnn.TenstorrentBackendOption(device=self.device)
         option.gen_graphviz = True
@@ -189,12 +86,13 @@ class TestModules(unittest.TestCase):
 
         # Check the graph has be rewritten and contain ttnn ops
         nodes = list(option._out_fx_graphs[0].nodes)
-        self.assertTrue(nodes[3].target == ttnn.reshape)
-        self.assertTrue(nodes[3].args[0].target == ttnn.to_device)
-        self.assertTrue(nodes[3].args[0].args[0].target == ttnn.from_torch)
-        self.assertTrue(nodes[4].target == ttnn.from_device)
-        self.assertTrue(nodes[5].target == ttnn.to_layout)
-        self.assertTrue(nodes[6].target == ttnn.to_torch)
+        self.assertTrue(nodes[1].target == ttnn.from_torch)
+        self.assertTrue(nodes[2].target == ttnn.to_layout)
+        self.assertTrue(nodes[3].target == ttnn.to_device)
+        self.assertTrue(nodes[4].target == ttnn.reshape)
+        self.assertTrue(nodes[5].target == ttnn.from_device)
+        self.assertTrue(nodes[6].target == ttnn.to_layout)
+        self.assertTrue(nodes[7].target == ttnn.to_torch)
         # Check inference result
         self.assertTrue(torch.allclose(result_before, result_after))
 
@@ -216,12 +114,13 @@ class TestModules(unittest.TestCase):
 
         # Check the graph has be rewritten and contain ttnn ops
         nodes = list(option._out_fx_graphs[0].nodes)
-        self.assertTrue(nodes[3].target == ttnn.permute)
-        self.assertTrue(nodes[3].args[0].target == ttnn.to_device)
-        self.assertTrue(nodes[3].args[0].args[0].target == ttnn.from_torch)
-        self.assertTrue(nodes[4].target == ttnn.from_device)
-        self.assertTrue(nodes[5].target == ttnn.to_layout)
-        self.assertTrue(nodes[6].target == ttnn.to_torch)
+        self.assertTrue(nodes[1].target == ttnn.from_torch)
+        self.assertTrue(nodes[2].target == ttnn.to_layout)
+        self.assertTrue(nodes[3].target == ttnn.to_device)
+        self.assertTrue(nodes[4].target == ttnn.permute)
+        self.assertTrue(nodes[5].target == ttnn.from_device)
+        self.assertTrue(nodes[6].target == ttnn.to_layout)
+        self.assertTrue(nodes[7].target == ttnn.to_torch)
         # Check inference result
         self.assertTrue(torch.allclose(result_before, result_after))
 
