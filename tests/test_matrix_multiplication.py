6f7cb6ccdafaf4489ee25f0d4ce97b8d70d5c35d
yoco <yoco@skymizer.com>
2024-04-09 10:31:38 +0000

    Support linear without bias

--- tests/test_matrix_multiplication.py
+++ tests/test_matrix_multiplication.py
@@ -53,7 +53,7 @@ class TestModules(unittest.TestCase):
         # Check inference result
         self.assertTrue(torch.allclose(result_before, result_after))
 
-    def test_linear(self):
+    def test_linear_with_bias(self):
         class LinearModule(torch.nn.Module):
             def __init__(self):
                 super().__init__()
@@ -93,5 +93,45 @@ class TestModules(unittest.TestCase):
         self.assertEqual(nodes[13 + 0].target, ttnn.from_device)
         self.assertEqual(nodes[13 + 1].target, ttnn.to_layout)
         self.assertEqual(nodes[13 + 2].target, ttnn.to_torch)
+        # Check inference result
+        self.assertTrue(torch.allclose(result_before, result_after))
+
+    def test_linear(self):
+        class LinearModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, x, y):
+                return torch.nn.functional.linear(x, y)
+
+            def input_shapes(self):
+                return [(32, 32), (32, 32)]
+
+        m = LinearModule()
+        input_shapes = m.input_shapes()
+        inputs = [
+            torch.randint(0, 3, shape).type(torch.bfloat16) for shape in input_shapes
+        ]
+        result_before = m.forward(*inputs)
+        option = torch_ttnn.TenstorrentBackendOption(device=self.device)
+        option.gen_graphviz = True
+        # The compilation is lazy, so we need to run forward once to trigger the compilation
+        m = torch.compile(m, backend=torch_ttnn.backend(option))
+        result_after = m.forward(*inputs)
+        self.assertEqual(1, len(option._out_fx_graphs))
+        option._out_fx_graphs[0].print_tabular()
+
+        # Check the graph has be rewritten and contain ttnn ops
+        nodes = list(option._out_fx_graphs[0].nodes)
+        self.assertEqual(nodes[2 + 0].target, ttnn.from_torch)
+        self.assertEqual(nodes[2 + 1].target, ttnn.to_layout)
+        self.assertEqual(nodes[2 + 2].target, ttnn.to_device)
+        self.assertEqual(nodes[5 + 0].target, ttnn.from_torch)
+        self.assertEqual(nodes[5 + 1].target, ttnn.to_layout)
+        self.assertEqual(nodes[5 + 2].target, ttnn.to_device)
+        self.assertEqual(nodes[8].target, ttnn.linear)
+        self.assertEqual(nodes[9 + 0].target, ttnn.from_device)
+        self.assertEqual(nodes[9 + 1].target, ttnn.to_layout)
+        self.assertEqual(nodes[9 + 2].target, ttnn.to_torch)
         # Check inference result
         self.assertTrue(torch.allclose(result_before, result_after))
--- torch_ttnn/patterns/linear.py
+++ torch_ttnn/patterns/linear.py
@@ -4,16 +4,27 @@ import torch
 from .. import wrap_ttnn_ops as wrap
 
 
-def linear_pat(x, y, z):
+def linear_with_bias_pat(x, y, z):
     a = torch.ops.aten.t.default(y)
     b = torch.ops.aten.addmm.default(z, x, a)
     return b
 
 
-def linear_rep(x, y, z):
+def linear_with_bias_rep(x, y, z):
     return wrap.linear(x, y, bias=z)
+
+
+def linear_pat(x, y):
+    a = torch.ops.aten.t.default(y)
+    b = torch.ops.aten.mm.default(x, a)
+    return b
+
+
+def linear_rep(x, y):
+    return wrap.linear(x, y)
 
 
 pat_rep_list = [
     (linear_pat, linear_rep),
+    (linear_with_bias_pat, linear_with_bias_rep),
 ]
