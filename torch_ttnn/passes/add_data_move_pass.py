7beaf96f19df3cc4cdb6a3738b3120c7160514c8
Kevin Wu <kevinwu@tenstorrent.com>
2024-04-12 14:29:45 -0500

    Move Dummy string repr to separate utils file

--- torch_ttnn/passes/add_data_move_pass.py
+++ torch_ttnn/passes/add_data_move_pass.py
@@ -1,4 +1,10 @@
 import torch
+from ..utils import (
+    DummyTtnnUint32,
+    DummyTtnnRowMajorLayout,
+    DummyTtnnTileLayout,
+    DummyDevice,
+)
 
 try:
     import ttnn
@@ -148,26 +154,6 @@ def try_add_data_move_out_for_split(src_node, dst_idx, dst_node) -> torch.fx.nod
     else:
         return None
 
-# See https://docs.google.com/document/d/1r2D4AagoeTRjEmXFnWzzafaWQkf-8hlIbX2ze-JAUFo/edit#heading=h.zad9rwqjv6cr
-class DummyDevice:
-    def __repr__(self):
-        return f"ttnn_Specified_Device"
-
-class DummyTtnnRowMajorLayout:
-    def __repr__(self):
-        return f"ttnn_ROW_MAJOR_LAYOUT"
-
-class DummyTtnnTileLayout:
-    def __repr__(self):
-        return f"ttnn_TILE_LAYOUT"
-
-class DummyTtnnUint32:
-    def __repr__(self):
-        return f"ttnn_uint32"
-
-class DummyTtnnBfloat16:
-    def __repr__(self):
-        return f"ttnn_bfloat16"
 
 class AddDataMovePass(PassBase):
     def call(self, gm: torch.fx.GraphModule):
--- torch_ttnn/utils.py
+++ torch_ttnn/utils.py
@@ -7,3 +7,23 @@ def GraphCleanup(gm: torch.fx.GraphModule) -> torch.fx.GraphModule:
 
     return gm
 
+# See https://docs.google.com/document/d/1r2D4AagoeTRjEmXFnWzzafaWQkf-8hlIbX2ze-JAUFo/edit#heading=h.zad9rwqjv6cr
+class DummyDevice:
+    def __repr__(self):
+        return f"ttnn_Specified_Device"
+
+class DummyTtnnRowMajorLayout:
+    def __repr__(self):
+        return f"ttnn_ROW_MAJOR_LAYOUT"
+
+class DummyTtnnTileLayout:
+    def __repr__(self):
+        return f"ttnn_TILE_LAYOUT"
+
+class DummyTtnnUint32:
+    def __repr__(self):
+        return f"ttnn_uint32"
+
+class DummyTtnnBfloat16:
+    def __repr__(self):
+        return f"ttnn_bfloat16"
