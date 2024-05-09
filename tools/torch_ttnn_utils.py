668679fe73409dfe10318aad83e37fec066edc26
swimdi <swimdi@skymizer.com>
2024-05-09 16:21:54 +0800

    Add unet_carvana

--- tools/torch_ttnn_utils.py
+++ tools/torch_ttnn_utils.py
@@ -67,6 +67,18 @@ class Monodepth2_depth(torch.nn.Module):
         outputs = self.depth_decoder(features)
         return outputs
 
+def print_map_location_note():
+    print(f"If raise 'torch.cuda.is_available() is False' \n\
+Then you should edit these code in {torch.hub.__file__} \n\
+def load_state_dict_from_url( \n\
+    url: str,\n\
+    model_dir: Optional[str] = None,\n\
+    map_location: MAP_LOCATION = None <= !!!change to 'cpu'!!!,\n\
+    progress: bool = True,\n\
+    check_hash: bool = False,\n\
+    file_name: Optional[str] = None,\n\
+    weights_only: bool = False,\n\
+)")
 
 def get_model_swimdi(model_name):
     if model_name == "monodepth2_depth":
@@ -76,21 +88,17 @@ def get_model_swimdi(model_name):
                               'deit_base_patch16_224',
                               pretrained=True)
     if model_name == "hardnet":
-        print(f"If raise torch.cuda.is_available() is False \n\
-Then you should edit the map_location: MAP_LOCATION = 'cpu' of load_state_dict_from_url in {torch.hub.__file__} \n\
-def load_state_dict_from_url( \n\
-    url: str,\n\
-    model_dir: Optional[str] = None,\n\
-    map_location: MAP_LOCATION = None <= change to 'cpu',\n\
-    progress: bool = True,\n\
-    check_hash: bool = False,\n\
-    file_name: Optional[str] = None,\n\
-    weights_only: bool = False,\n\
-)")
+        print_map_location_note()
         return torch.hub.load('PingoLH/Pytorch-HarDNet',
                               'hardnet68',
                                map_location=torch.device('cpu'),
                                pretrained=True)
+    if model_name == "unet_carvana":
+        print_map_location_note()
+        return torch.hub.load('milesial/Pytorch-UNet',
+                              'unet_carvana',
+                              pretrained=True,
+                              scale=0.5)
     return None
 
 
