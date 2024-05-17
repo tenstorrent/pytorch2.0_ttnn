861c0ada12783f8c03f19f331008eab51d343137
yoco <yoco@skymizer.com>
2024-05-17 07:43:27 +0000

    Update test_stat import statment

--- tests/test_stat.py
+++ tests/test_stat.py
@@ -1,10 +1,12 @@
 import os
 import shutil
 import torch
+
 # from torch_ttnn import torch_stat
-from torch_ttnn.tracer import Tracer
+from tracer.tracer import Tracer
 import unittest
 from torch._dynamo.backends.common import aot_autograd
+
 
 class ConvModule(torch.nn.Module):
     def __init__(self):
@@ -48,11 +50,15 @@ class TestModules(unittest.TestCase):
         input_shapes = m.input_shapes()
         inputs = [torch.rand(shape, requires_grad=True) for shape in input_shapes]
 
-        aot_backend = aot_autograd(fw_compiler = Tracer(self.dummy_backend, "fw_conv", self.out_folder))
+        aot_backend = aot_autograd(
+            fw_compiler=Tracer(self.dummy_backend, "fw_conv", self.out_folder)
+        )
         m = torch.compile(m, backend=aot_backend)
         _ = m.forward(*inputs)
 
-        fw_result_json_path = os.path.join(self.out_folder, "raw", "fw_conv_orig_0.json")
+        fw_result_json_path = os.path.join(
+            self.out_folder, "raw", "fw_conv_orig_0.json"
+        )
         self.assertTrue(os.path.isfile(fw_result_json_path))
 
     def test_conv_with_backward(self):
@@ -60,14 +66,20 @@ class TestModules(unittest.TestCase):
         input_shapes = m.input_shapes()
         inputs = [torch.rand(shape, requires_grad=True) for shape in input_shapes]
 
-        aot_backend = aot_autograd(fw_compiler = Tracer(self.dummy_backend, "fw_conv", self.out_folder),
-                                   bw_compiler = Tracer(self.dummy_backend, "bw_conv", self.out_folder))
+        aot_backend = aot_autograd(
+            fw_compiler=Tracer(self.dummy_backend, "fw_conv", self.out_folder),
+            bw_compiler=Tracer(self.dummy_backend, "bw_conv", self.out_folder),
+        )
         m = torch.compile(m, backend=aot_backend)
         result = m.forward(*inputs)
         result.backward(torch.ones_like(result))
 
-        fw_result_json_path = os.path.join(self.out_folder, "raw", "fw_conv_orig_0.json")
-        bw_result_json_path = os.path.join(self.out_folder, "raw", "bw_conv_orig_0.json")
+        fw_result_json_path = os.path.join(
+            self.out_folder, "raw", "fw_conv_orig_0.json"
+        )
+        bw_result_json_path = os.path.join(
+            self.out_folder, "raw", "bw_conv_orig_0.json"
+        )
         self.assertTrue(os.path.isfile(fw_result_json_path))
         self.assertTrue(os.path.isfile(bw_result_json_path))
 
@@ -76,11 +88,19 @@ class TestModules(unittest.TestCase):
         input_shapes = m.input_shapes()
         inputs = [torch.rand(shape, requires_grad=True) for shape in input_shapes]
 
-        aot_backend = aot_autograd(fw_compiler = Tracer(self.dummy_backend, "fw_conv", self.out_folder, trace_modi=True))
+        aot_backend = aot_autograd(
+            fw_compiler=Tracer(
+                self.dummy_backend, "fw_conv", self.out_folder, trace_modi=True
+            )
+        )
         m = torch.compile(m, backend=aot_backend)
         _ = m.forward(*inputs)
 
-        fw_result_json_path = os.path.join(self.out_folder, "raw", "fw_conv_orig_0.json")
-        fw_modi_result_json_path = os.path.join(self.out_folder, "raw", "fw_conv_modi_0.json")
+        fw_result_json_path = os.path.join(
+            self.out_folder, "raw", "fw_conv_orig_0.json"
+        )
+        fw_modi_result_json_path = os.path.join(
+            self.out_folder, "raw", "fw_conv_modi_0.json"
+        )
         self.assertTrue(os.path.isfile(fw_result_json_path))
         self.assertTrue(os.path.isfile(fw_modi_result_json_path))
