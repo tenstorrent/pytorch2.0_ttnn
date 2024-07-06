05a9594f1b4a750a5ccd69c7dfd8124f3a6d11d1
Artem Yerofieiev <artem@tenstorrent.com>
2024-07-06 01:39:16 +0000

    fix

--- tests/tools/test_stats.py
+++ tests/tools/test_stats.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import torch
-import torch_stat
+from torch_ttnn import torch_stat
 import unittest
 import json
 from torch.fx.passes.dialect.common.cse_pass import CSEPass
