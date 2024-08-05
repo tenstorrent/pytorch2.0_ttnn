05dbe1817ca26acbc06b41028eafefd11f0cca52
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-06 03:26:18 +0800

    Use incoming test_fall_back.py

--- tests/test_fall_back.py
+++ tests/test_fall_back.py
@@ -1,14 +1,9 @@
 import torch
 import torch_ttnn
-<<<<<<< HEAD
-import ttnn
-import unittest
-=======
 import pytest
 import ttnn
 
 from tests.utils import check_with_pcc
->>>>>>> upstream/main
 
 
 @pytest.mark.parametrize(
@@ -47,45 +42,6 @@ def test_fall_back(device, input_shapes):
     assert target.count(ttnn.add) == 1
     assert target.count(ttnn.matmul) == 1
 
-<<<<<<< HEAD
-class TestModules(unittest.TestCase):
-    def setUp(self):
-        # Open device 0
-        self.device: ttnn.Device = ttnn.open_device(device_id=0)
-
-    def tearDown(self):
-        # Close the device
-        ttnn.close_device(self.device)
-
-    def test_fall_back(self):
-        m = MixModule()
-        input_shapes = m.input_shapes()
-        inputs = [torch.rand(shape, dtype=torch.bfloat16) for shape in input_shapes]
-        result_before = m.forward(*inputs)
-        option = torch_ttnn.TenstorrentBackendOption(device=self.device)
-        option.gen_graphviz = True
-        # The compilation is lazy, so we need to run forward once to trigger the compilation
-        m = torch.compile(m, backend="ttnn", options=option)
-        result_after = m.forward(*inputs)
-        self.assertEqual(1, len(option._out_fx_graphs))
-        option._out_fx_graphs[0].print_tabular()
-
-        # Check the graph has be rewritten and contain ttnn ops
-        nodes = list(option._out_fx_graphs[0].nodes)
-        self.assertEqual(nodes[2].target, torch.ops.aten.div.Tensor)
-        self.assertEqual(nodes[3 + 0].target, ttnn.from_torch)
-        self.assertEqual(nodes[3 + 1].target, ttnn.to_layout)
-        self.assertEqual(nodes[3 + 2].target, ttnn.to_device)
-        self.assertEqual(nodes[3 + 3].target, ttnn.to_layout)
-        self.assertEqual(nodes[3 + 4].target, ttnn.to_device)
-        self.assertEqual(nodes[8].target, ttnn.add)
-        self.assertEqual(nodes[9].target, ttnn.matmul)
-        self.assertEqual(nodes[10 + 0].target, ttnn.from_device)
-        self.assertEqual(nodes[10 + 1].target, ttnn.to_layout)
-        self.assertEqual(nodes[10 + 2].target, ttnn.to_torch)
-        # Check inference result
-        self.assertTrue(torch.allclose(result_before, result_after))
-=======
     nodes = list(option._out_fx_graphs[-1].nodes)
     reciprocal_idx = [i for i, n in enumerate(target) if n == ttnn.reciprocal]
     multiply_idx = [i for i, n in enumerate(target) if n == ttnn.multiply]
@@ -104,4 +60,3 @@ class TestModules(unittest.TestCase):
 
     # Check inference result
     assert check_with_pcc(result_before, result_after)
->>>>>>> upstream/main
