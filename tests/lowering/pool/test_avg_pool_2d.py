17c3583821639b4e201f44ad6f5c3cabd9245f8e
Kevin Wu <kevinwu@tenstorrent.com>
2024-08-12 22:05:19 +0000

    Fix avg_pool_2d test

--- tests/lowering/pool/test_avg_pool_2d.py
+++ tests/lowering/pool/test_avg_pool_2d.py
@@ -16,21 +16,24 @@ class AdaptiveAvgPool2dModule(torch.nn.Module):
 
 @pytest.mark.parametrize(
     "input_shapes",
-    [[(1, 2048, 7, 7)]],
+    [(1, 2048, 7, 7)],
 )
 def test_adaptive_avg_pool_2d(device, input_shapes):
     m = AdaptiveAvgPool2dModule()
-    inputs = [torch.rand(shape, dtype=torch.bfloat16) for shape in input_shapes]
-    result_before = m.forward(*inputs)
+    inputs = torch.rand(input_shapes, dtype=torch.bfloat16)
+    result_before = m.forward(inputs)
     option = torch_ttnn.TorchTtnnOption(device=device)
     option.gen_graphviz = True
     # The compilation is lazy, so we need to run forward once to trigger the compilation
     m = torch.compile(m, backend=torch_ttnn.backend, options=option)
-    result_after = m.forward(*inputs)
+    # ttnn operates on channels-last tensors
+    input_tensor = torch.permute(inputs, (0, 2, 3, 1))
+    result_after = m.forward(input_tensor)
+    result_after = torch.permute(result_after, (0, 3, 1, 2))
     option._out_fx_graphs[0].print_tabular()
 
     # Check the graph has be rewritten and contain ttnn ops
     nodes = list(option._out_fx_graphs[0].nodes)
     assert [node.target for node in nodes].count(ttnn.global_avg_pool2d) == 1
     # Check inference result
-    assert_with_pcc(result_before, result_after)
+    assert_with_pcc(result_before, result_after, pcc=0.99)
