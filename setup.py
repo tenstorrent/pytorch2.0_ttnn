be9c0322160c82b3a9eb943075e824ca22d430a3
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-13 08:33:28 +0800

    Update metadata in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ setup(
     name="torch-ttnn",
     version="0.1.0",
     description="PyTorch dynamo backend for Tenstorrent TT-NN framework",
-    author="yoco",
-    author_email="yoco@skymizer.com",
     license="MIT",
-    url="https://git.skymizer.com/tenstorrent-metal/pytorch2.0_ttnn",
+    url="https://github.com/tenstorrent/pytorch2.0_ttnn",
     packages=["torch_ttnn"],
     install_requires=[],
 )
