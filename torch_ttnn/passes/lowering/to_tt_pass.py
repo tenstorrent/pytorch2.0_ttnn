0101b8ac3c7ec96846a01bd2046c868385ea2744
Kevin Wu <kevinwu@tenstorrent.com>
2024-08-21 00:25:14 +0000

    Restrict lowering to ttnn.arange if steps are negative

--- torch_ttnn/passes/lowering/to_tt_pass.py
+++ torch_ttnn/passes/lowering/to_tt_pass.py
@@ -242,7 +242,8 @@ def ReplaceMoreTtManually(gm: torch.fx.GraphModule) -> torch.fx.GraphModule:
                     new_nodes.append(new_node)
             if node.target == torch.ops.aten.arange.start_step:
                 # NOTE(kevinwuTT): ttnn.arange does not support starting values smaller than 2 currently
-                if args[0] >= 2:
+                # ttnn.arange also doesn't support negative steps
+                if args[0] >= 2 and args[2] > 0:
                     new_args = (args[0],)
                     new_kwargs = {
                         "end": args[1],
