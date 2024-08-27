839d13973381cb5b335e566ad6b07199ed589d07
GitHub Action <action@github.com>
2024-08-27 02:12:14 +0000

    [auto] Pre-commit run on all files

--- tests/models/whisper/test_whisper.py
+++ tests/models/whisper/test_whisper.py
@@ -13,7 +13,9 @@ def test_whisper(record_property):
     # load dummy dataset and read audio files
     ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
     sample = ds[0]["audio"]
-    input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
+    input_features = processor(
+        sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt"
+    ).input_features
 
     # generate token ids
     predicted_ids = model.generate(input_features)
