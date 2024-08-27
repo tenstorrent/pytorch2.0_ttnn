6bab8ecd6db36e704e329f7db59661d0cef07fd2
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-27 15:32:27 +0000

    Test conversion from `aten.ne.Scalar` to `ttnn.ne`

--- tests/lowering/eltwise/binary/test_ne.py
+++ tests/lowering/eltwise/binary/test_ne.py
@@ -34,3 +34,33 @@ def test_ne(device, input_shapes):
 
     # Check inference result
     assert torch.allclose(result_before, result_after.to(torch.bool))
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    [[(64, 128)]],
+)
+def test_ne_scalar(device, input_shapes):
+    m = NeModule()
+    inputs = [torch.rand(shape, dtype=torch.bfloat16) for shape in input_shapes]
+    scalar = inputs[0][0][0].item()
+    result_before = m.forward(inputs[0], scalar)
+    option = torch_ttnn.TorchTtnnOption(device=device)
+    option.gen_graphviz = True
+    # The compilation is lazy, so we need to run forward once to trigger the compilation
+    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
+    result_after = m.forward(inputs[0], scalar)
+    option._out_fx_graphs[0].print_tabular()
+
+    # Check the graph has be rewritten and contain ttnn ops
+    nodes = list(option._out_fx_graphs[0].nodes)
+    target = [node.target for node in nodes]
+    assert target.count(ttnn.full) == 1
+    assert target.count(ttnn.ne) == 1
+    assert target.index(ttnn.full) < target.index(ttnn.ne)
+    # Intermediate node meta check if preserved
+    for node in nodes:
+        if node.target == ttnn.full:
+            assert node.meta["val"].size() == input_shapes[0]
+    # Check inference result
+    assert torch.allclose(result_before, result_after.to(torch.bool))
