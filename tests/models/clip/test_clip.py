f8f5c37f3f71b6a83484f716ea4b1447354f88db
cthsieh <cthsieh@skymizer.com>
2024-08-27 13:52:33 +0000

    Set xfail to the compiled part in the test.

--- tests/models/clip/test_clip.py
+++ tests/models/clip/test_clip.py
@@ -1,19 +1,16 @@
 from PIL import Image
 import requests
 import torch
-
 from transformers import CLIPProcessor, CLIPModel
+import pytest
 
 
+@pytest.mark.torch_only
 def test_clip(record_property):
     record_property("model_name", "CLIP")
 
-    model = CLIPModel.from_pretrained(
-        "openai/clip-vit-base-patch32", torch_dtype=torch.bfloat16
-    )
-    processor = CLIPProcessor.from_pretrained(
-        "openai/clip-vit-base-patch32", torch_dtype=torch.bfloat16
-    )
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32", torch_dtype=torch.bfloat16)
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", torch_dtype=torch.bfloat16)
 
     url = "http://images.cocodataset.org/val2017/000000039769.jpg"
     image = Image.open(requests.get(url, stream=True).raw)
@@ -26,11 +23,7 @@ def test_clip(record_property):
     )
 
     outputs = model(**inputs)
-    logits_per_image = (
-        outputs.logits_per_image
-    )  # this is the image-text similarity score
-    probs = logits_per_image.softmax(
-        dim=1
-    )  # we can take the softmax to get the label probabilities
+    logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
+    probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
 
     record_property("torch_ttnn", (model, inputs, outputs))
