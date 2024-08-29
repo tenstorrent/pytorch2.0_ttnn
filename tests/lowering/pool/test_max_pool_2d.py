ec452bf93bc3eae9b35ddd85cbfb0b32d99b4b8f
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-29 17:22:15 +0000

    Fix test for max_pool_2d with indices

--- tests/lowering/pool/test_max_pool_2d.py
+++ tests/lowering/pool/test_max_pool_2d.py
@@ -15,24 +15,19 @@ class MaxPool2dModule(torch.nn.Module):
 
 
 @pytest.mark.parametrize(
-    "input_shapes, kernel_size, return_indices",
-    (
-        ((1, 3, 64, 64), (3, 3), False),
-        ((1, 3, 64, 64), (3, 3), True),
-    ),
+    "input_shapes, kernel_size",
+    [((1, 3, 64, 64), (3, 3))],
 )
-def test_max_pool_2d(device, input_shapes, kernel_size, return_indices):
+def test_max_pool_2d_plain(device, input_shapes, kernel_size):
     m = MaxPool2dModule()
     inputs = torch.rand(input_shapes, dtype=torch.bfloat16)
-    args = (kernel_size,)
-    kwargs = {"return_indices": return_indices}
-    result_before = m.forward(inputs, *args, **kwargs)
+    result_before = m.forward(inputs, kernel_size)
     option = torch_ttnn.TorchTtnnOption(device=device)
     # The compilation is lazy, so we need to run forward once to trigger the compilation
     m = torch.compile(m, backend=torch_ttnn.backend, options=option)
     # ttnn operates on channels-last tensors
     input_tensor = torch.permute(inputs, (0, 2, 3, 1))
-    result_after = m.forward(input_tensor, *args, **kwargs)
+    result_after = m.forward(input_tensor, kernel_size)
     result_after = torch.permute(result_after, (0, 3, 1, 2))
     option._out_fx_graphs[0].print_tabular()
 
@@ -41,3 +36,28 @@ def test_max_pool_2d(device, input_shapes, kernel_size, return_indices):
     assert [node.target for node in nodes].count(ttnn.max_pool2d) == 1
     # Check inference result
     assert_with_pcc(result_before, result_after, pcc=0.99)
+
+
+@pytest.mark.parametrize(
+    "input_shapes, kernel_size",
+    [((1, 3, 64, 64), (3, 3))],
+)
+def test_max_pool_2d_with_indices(device, input_shapes, kernel_size):
+    m = MaxPool2dModule()
+    inputs = torch.rand(input_shapes, dtype=torch.bfloat16)
+    (pool_before, argmax_before) = m.forward(inputs, kernel_size, return_indices=True)
+    option = torch_ttnn.TorchTtnnOption(device=device)
+    # The compilation is lazy, so we need to run forward once to trigger the compilation
+    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
+    # ttnn operates on channels-last tensors
+    input_tensor = torch.permute(inputs, (0, 2, 3, 1))
+    (pool_after, argmax_after) = m.forward(input_tensor, kernel_size, return_indices=True)
+    pool_after = torch.permute(pool_after, (0, 3, 1, 2))
+    option._out_fx_graphs[0].print_tabular()
+
+    # Check the graph has be rewritten and contain ttnn ops
+    nodes = list(option._out_fx_graphs[0].nodes)
+    assert [node.target for node in nodes].count(ttnn.max_pool2d) == 1
+    # Check inference result
+    assert_with_pcc(pool_before, pool_after, pcc=0.99)
+    assert_with_pcc(argmax_before, argmax_after)
