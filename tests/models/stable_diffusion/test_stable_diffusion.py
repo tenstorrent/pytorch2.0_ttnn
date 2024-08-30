c4a4e8a6166ce57b24e9e9b444ccf5956f2847cc
cthsieh <cthsieh@skymizer.com>
2024-08-30 15:33:28 +0000

    Fix Hugging Face path to model.
    However, this test still cannot by handled by Dynamo because the
    model is used via pipeline. We marked it as skipped for future
    study.

--- tests/models/stable_diffusion/test_stable_diffusion.py
+++ tests/models/stable_diffusion/test_stable_diffusion.py
@@ -4,11 +4,11 @@ import torch
 import pytest
 
 
-@pytest.mark.skip(reason="runwayml/stable-diffusion-v1-5 is no longer available to download from huggingface")
+@pytest.mark.skip(reason="Dynamo cannot support pipeline.")
 def test_stable_diffusion(record_property):
     record_property("model_name", "Stable Diffusion")
 
-    model_id = "runwayml/stable-diffusion-v1-5"
+    model_id = "CompVis/stable-diffusion-v1-4"
     pipe = StableDiffusionPipeline.from_pretrained(model_id)
 
     prompt = "a photo of an astronaut riding a horse on mars"
