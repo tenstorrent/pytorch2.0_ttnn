79936f2437de23793d05078edc7d26144216e646
Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>
2024-08-30 09:58:02 -0700

    Update huggingface_delete_cache.py

--- tools/huggingface_delete_cache.py
+++ tools/huggingface_delete_cache.py
@@ -2,9 +2,7 @@ from huggingface_hub import scan_cache_dir
 
 
 cache_info = scan_cache_dir()
-all_revisions = []
-for repo in cache_info.repos:
-    all_revisions.extend(repo.revisions)
+all_revisions = [revision.commit_hash for repo in cache_info.repos for revision in repo.revisions]
 
 delete_strategy = cache_info.delete_revisions(*all_revisions)
 print("Will free " + delete_strategy.expected_freed_size_str)
