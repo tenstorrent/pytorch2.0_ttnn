a3514509a7e54719154edd7a8bc09bf25f33f4bc
cthsieh <cthsieh@skymizer.com>
2024-09-01 02:36:22 +0000

    Add model YOLOv5.

---
+++ tests/models/yolov5/test_yolov5.py
@@ -0,0 +1,31 @@
+import torch
+from PIL import Image
+from torchvision import transforms
+import requests
+import pytest
+
+dependencies = ["ultralytics", "ultralytics-thop"]
+
+
+@pytest.mark.usefixtures("manage_dependencies")
+@pytest.mark.compilation_xfail
+def test_yolov5(record_property):
+    record_property("model_name", "YOLOv5")
+
+    """
+    The model is from https://pytorch.org/hub/ultralytics_yolov5/
+    """
+    # Model
+    model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True, autoshape=False, device="cpu")
+
+    # Image preprocessing
+    image_url = "https://raw.githubusercontent.com/pytorch/hub/master/images/dog.jpg"
+    image = Image.open(requests.get(image_url, stream=True).raw)
+    transform = transforms.Compose([transforms.Resize((512, 512)), transforms.ToTensor()])
+    img_tensor = [transform(image).unsqueeze(0)]
+    batch_tensor = torch.cat(img_tensor, dim=0)
+
+    # Inference
+    results = model(batch_tensor)
+
+    record_property("torch_ttnn", (model, batch_tensor, results))
