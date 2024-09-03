f71c2a002ac169e34e72e718b283e9264f799007
Chen-Pang He <chen.pang.he@jdh8.org>
2024-09-03 16:46:00 +0000

    Generalize test for mean for mean.default

--- tests/lowering/reduction/test_mean.py
+++ tests/lowering/reduction/test_mean.py
@@ -6,12 +6,12 @@ import ttnn
 from tests.utils import assert_with_pcc
 
 
-class MeanDimModule(torch.nn.Module):
+class MeanModule(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, input, dim, keepdim=False):
-        return torch.mean(input, dim, keepdim)
+    def forward(self, *args, **kwargs):
+        return torch.mean(*args, **kwargs)
 
 
 @pytest.mark.parametrize(
@@ -19,15 +19,14 @@ class MeanDimModule(torch.nn.Module):
     [((1, 32, 32), -1)],
 )
 def test_mean_dim(device, input_shape, dim):
-    m = MeanDimModule()
+    m = MeanModule()
     input = torch.zeros(input_shape, dtype=torch.bfloat16).uniform_(-1, 1)
-    keepdim = True
-    result_before = m.forward(input, dim, keepdim)
+    result_before = m.forward(input, dim, keepdim=True)
     option = torch_ttnn.TorchTtnnOption(device=device)
     option.gen_graphviz = True
     # The compilation is lazy, so we need to run forward once to trigger the compilation
     m = torch.compile(m, backend=torch_ttnn.backend, options=option)
-    result_after = m.forward(input, dim, keepdim)
+    result_after = m.forward(input, dim, keepdim=True)
     option._out_fx_graphs[0].print_tabular()
 
     # Check the graph has be rewritten and contain ttnn ops
