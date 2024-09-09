6feb077a7515eaa26e77bd82c6894a8384a58abc
cthsieh <cthsieh@skymizer.com>
2024-09-09 12:23:54 +0000

    Prepare model locally instead of downloading from the GitHub.

--- tests/models/unet_carvana/test_unet_carvana.py
+++ tests/models/unet_carvana/test_unet_carvana.py
@@ -8,34 +8,11 @@ import tempfile
 import torch
 import pytest
 
-
-def github_download(repo_name):
-    repo_url = "https://github.com/" + repo_name + ".git"
-
-    cache_dir = Path(tempfile.gettempdir()) / "github_repos"
-    repo_dir = cache_dir / repo_name
-
-    # Create the cache directory if it doesn't exist
-    cache_dir.mkdir(parents=True, exist_ok=True)
-
-    # Check if the repository has already been cloned
-    if not repo_dir.exists():
-        subprocess.run(["git", "clone", repo_url, str(repo_dir)], check=True)
-    else:
-        print(f"Repository {repo_name} already exists. Skipping clone.")
-
-    return repo_dir
+from tests.models.unet_carvana.carvana_unet_segmentation.model import UNET
 
 
-@pytest.mark.compilation_xfail
 def test_unet_carvana(record_property):
     record_property("model_name", "Unet-carvana")
-
-    repo_dir = github_download("arief25ramadhan/carvana-unet-segmentation")
-
-    # Import the module from the cloned repo
-    sys.path.insert(0, str(repo_dir))
-    from model import UNET
 
     model = UNET(in_channels=3, out_channels=1)
 
