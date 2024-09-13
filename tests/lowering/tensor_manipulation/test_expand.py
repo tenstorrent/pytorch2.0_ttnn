e16f1a7acb3da18eda5f6011e8e398b33dbfdefd
Chen-Pang He <chen.pang.he@jdh8.org>
2024-09-13 18:09:37 +0000

    Test conversion of `aten.expand` with known input variations

--- tests/lowering/tensor_manipulation/test_expand.py
+++ tests/lowering/tensor_manipulation/test_expand.py
@@ -2,10 +2,8 @@ import torch
 import torch_ttnn
 import pytest
 import ttnn
-from torch_ttnn.utils import (
-    TtnnRowMajorLayout,
-    TtnnTileLayout,
-)
+from torch_ttnn.utils import TtnnRowMajorLayout, TtnnTileLayout
+from torch_ttnn import target_wrappers
 
 
 class ExpandModule(torch.nn.Module):
@@ -16,30 +14,29 @@ class ExpandModule(torch.nn.Module):
         return x.expand(new_shape)
 
 
-class ExpandAfterOpModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x, new_shape):
-        a = torch.clone(x)
-        return a.expand(new_shape)
+ALIGNMENT = pytest.mark.xfail(
+    reason="Current repeat implementation requires aligned last dim when repeating on last dim"
+)
 
 
-class ExpandBetweenOpsModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x, new_shape):
-        a = torch.clone(x)
-        ex = a.expand(new_shape)
-        return torch.add(ex, ex)
+PAGE_SIZE = pytest.mark.xfail(reason="For interleaved-buffers page size should be divisible by buffer size")
 
 
 @pytest.mark.parametrize(
     "input_shape, new_shape",
-    [
-        ((1, 4), (4, 4)),
-    ],
+    (
+        pytest.param((1, 1, 1, 16, 1), (1, 1, 1, 16, 2), marks=ALIGNMENT),
+        ((1, 1, 1, 16), (1, 12, 16, 16)),
+        pytest.param((1, 1, 1, 19), (1, 1, 19, 19), marks=PAGE_SIZE),
+        ((1, 1, 1, 24), (1, 1, 1, 24)),
+        ((1, 1, 1, 24), (1, 1, 24, 24)),
+        ((1, 1, 1, 32), (1, 1, 32, 32)),
+        pytest.param((1, 1, 1, 45), (1, 1, 45, 45), marks=PAGE_SIZE),
+        ((1, 1, 1, 46), (1, 1, 1, 46)),
+        ((1, 1, 1024), (1, -1, -1)),
+        ((1, 1, 12, 16, 2), (1, 1, -1, -1, -1)),
+        pytest.param((1, 1, 7, 7), (2, 1, 7, 7), marks=PAGE_SIZE),
+    ),
 )
 def test_expand(device, input_shape, new_shape):
     m = ExpandModule()
@@ -56,119 +53,7 @@ def test_expand(device, input_shape, new_shape):
     # Check the graph has be rewritten and contain ttnn ops
     nodes = list(option._out_fx_graphs[0].nodes)
     target = [node.target for node in nodes]
-    assert target.count(ttnn.repeat) == 1
-    assert nodes[target.index(ttnn.repeat)].args[1].target == ttnn.Shape
-    # Check inference result
-    assert torch.allclose(result_before, result_after, rtol=0.2)
-
-
-@pytest.mark.parametrize(
-    "input_shape, new_shape",
-    [
-        ((1, 4), (4, 4)),
-    ],
-)
-def test_expand_after_op(device, input_shape, new_shape):
-    m = ExpandAfterOpModule()
-    tensor = torch.rand(input_shape, dtype=torch.bfloat16)
-    inputs = [tensor, new_shape]
-    result_before = m.forward(*inputs)
-    option = torch_ttnn.TorchTtnnOption(device=device)
-    option.gen_graphviz = True
-    # The compilation is lazy, so we need to run forward once to trigger the compilation
-    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
-    result_after = m.forward(*inputs)
-    option._out_fx_graphs[0].print_tabular()
-
-    # Check the graph has be rewritten and contain ttnn ops
-    nodes = list(option._out_fx_graphs[0].nodes)
-    target = [node.target for node in nodes]
-    assert target.count(ttnn.repeat) == 1
-    repeat_node = nodes[target.index(ttnn.repeat)]
-    assert repeat_node.args[0].target == ttnn.to_layout
-    assert repeat_node.args[0].args[0].target == ttnn.clone
-    assert type(repeat_node.args[0].args[1]) is type(TtnnRowMajorLayout())
-    assert repeat_node.args[1].target == ttnn.Shape
-    # Check inference result
-    assert torch.allclose(result_before, result_after, rtol=0.2)
-
-
-@pytest.mark.parametrize(
-    "input_shape, new_shape",
-    [
-        ((1, 4), (4, 4)),
-    ],
-)
-def test_expand_before_op(device, input_shape, new_shape):
-    class ExpandBeforeOpModule(torch.nn.Module):
-        def __init__(self):
-            super().__init__()
-
-        def forward(self, x, new_shape):
-            ex = x.expand(new_shape)
-            return torch.add(ex, ex)
-
-    m = ExpandBeforeOpModule()
-    tensor = torch.rand(input_shape, dtype=torch.bfloat16)
-    inputs = [tensor, new_shape]
-    result_before = m.forward(*inputs)
-    option = torch_ttnn.TorchTtnnOption(device=device)
-    option.gen_graphviz = True
-    # The compilation is lazy, so we need to run forward once to trigger the compilation
-    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
-    result_after = m.forward(*inputs)
-    option._out_fx_graphs[0].print_tabular()
-
-    # Check the graph has be rewritten and contain ttnn ops
-    nodes = list(option._out_fx_graphs[0].nodes)
-    target = [node.target for node in nodes]
-    assert target.count(ttnn.repeat) == 1
-    assert nodes[target.index(ttnn.repeat)].args[1].target == ttnn.Shape
-    # to_layout that follows ttnn.repeat
-    to_layout_idx = target.index(ttnn.to_layout, target.index(ttnn.repeat))
-    to_layout_node = nodes[to_layout_idx]
-    assert to_layout_node.args[0].target == ttnn.repeat
-    assert type(to_layout_node.args[1]) is type(TtnnTileLayout())
-    assert target.count(ttnn.add) == 1
-    assert to_layout_idx < target.index(ttnn.add)
-
-    # Check inference result
-    assert torch.allclose(result_before, result_after, rtol=0.2)
-
-
-@pytest.mark.parametrize(
-    "input_shape, new_shape",
-    [
-        ((1, 4), (4, 4)),
-    ],
-)
-def test_expand_between_ops(device, input_shape, new_shape):
-    m = ExpandBetweenOpsModule()
-    tensor = torch.rand(input_shape, dtype=torch.bfloat16)
-    inputs = [tensor, new_shape]
-    result_before = m.forward(*inputs)
-    option = torch_ttnn.TorchTtnnOption(device=device)
-    option.gen_graphviz = True
-    # The compilation is lazy, so we need to run forward once to trigger the compilation
-    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
-    result_after = m.forward(*inputs)
-    option._out_fx_graphs[0].print_tabular()
-
-    # Check the graph has be rewritten and contain ttnn ops
-    nodes = list(option._out_fx_graphs[0].nodes)
-    target = [node.target for node in nodes]
-    assert target.count(ttnn.repeat) == 1
-    repeat_node = nodes[target.index(ttnn.repeat)]
-    assert repeat_node.args[0].target == ttnn.to_layout
-    assert repeat_node.args[0].args[0].target == ttnn.clone
-    assert type(repeat_node.args[0].args[1]) is type(TtnnRowMajorLayout())
-    assert repeat_node.args[1].target == ttnn.Shape
-    # to_layout that follows ttnn.repeat
-    to_layout_idx = target.index(ttnn.to_layout, target.index(ttnn.repeat))
-    to_layout_node = nodes[to_layout_idx]
-    assert to_layout_node.args[0].target == ttnn.repeat
-    assert type(to_layout_node.args[1]) is type(TtnnTileLayout())
-    assert target.count(ttnn.add) == 1
-    assert to_layout_idx < target.index(ttnn.add)
+    non_trivial = input_shape != result_before.shape
+    assert target.count(target_wrappers.repeat) == non_trivial
     # Check inference result
     assert torch.allclose(result_before, result_after, rtol=0.2)
