9f7184a8bf7c2ba60cefc677d305d471f3191902
Chen-Pang He <chen.pang.he@jdh8.org>
2024-09-16 16:55:26 +0000

    Use the well known test cases

--- tests/lowering/pool/test_max_pool_2d.py
+++ tests/lowering/pool/test_max_pool_2d.py
@@ -11,22 +11,27 @@ class MaxPool2dModule(torch.nn.Module):
         super().__init__()
 
     def forward(self, *args, **kwargs):
-        return torch.nn.functional.max_pool2d(*args, **kwargs, stride=2)
+        return torch.nn.functional.max_pool2d(*args, **kwargs)
 
 
 @pytest.mark.parametrize(
-    "input_shapes, kernel_size",
-    [((1, 16, 64, 64), (3, 3))],
+    "input_shapes, kernel_size, stride, padding, dilation",
+    (
+        ((1, 1024, 17, 17), (3, 3), 2, 0, 1),
+        ((1, 128, 112, 112), (2, 2), 2, 0, 1),
+        ((1, 192, 28, 28), (3, 3), 1, 1, 1),  # ceil_mode=True?
+        ((1, 512, 19, 19), (3, 3), 1, 1, 1),
+    ),
 )
-def test_max_pool_2d_plain(device, input_shapes, kernel_size):
+def test_max_pool_2d_plain(device, input_shapes, kernel_size, stride, padding, dilation):
     m = MaxPool2dModule()
     input_tensor = torch.rand(input_shapes, dtype=torch.bfloat16)
-    result_before = m.forward(input_tensor, kernel_size)
+    result_before = m.forward(input_tensor, kernel_size, stride, padding, dilation)
     option = torch_ttnn.TorchTtnnOption(device=device)
     # The compilation is lazy, so we need to run forward once to trigger the compilation
     m = torch.compile(m, backend=torch_ttnn.backend, options=option)
     # ttnn operates on channels-last tensors
-    result_after = m.forward(input_tensor, kernel_size)
+    result_after = m.forward(input_tensor, kernel_size, stride, padding, dilation)
     option._out_fx_graphs[0].print_tabular()
 
     # Check the graph has be rewritten and contain ttnn ops
