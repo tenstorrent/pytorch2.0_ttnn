14d2071c4720722e9ccad74a2b20cc5a9669b4fe
mcw-zwakeelTT <mcw-zwakeel@tenstorrent.com>
2024-09-16 20:27:38 +0000

    Handle non-4d slice inputs; slice in row major

--- tests/lowering/tensor_manipulation/test_slice.py
+++ tests/lowering/tensor_manipulation/test_slice.py
@@ -12,6 +12,14 @@ class SliceModule(torch.nn.Module):
         return input[:, :, start:end]
 
 
+class SliceModule2(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input, dim, start, end):
+        return torch.ops.aten.slice.Tensor(input, dim, start, end)
+
+
 @pytest.mark.parametrize(
     "input_shape, start, end",
     (
@@ -35,4 +43,35 @@ def test_slice(device, input_shape, start, end):
     nodes = list(option._out_fx_graphs[0].nodes)
     assert [node.target for node in nodes].count(ttnn.slice) == 1
     # Check inference result
+    print(f"result_before_shape:{result_before.shape}\nresult_after_shape:{result_after.shape}")
+    print(f"result_before:{result_before}\nresult_after:{result_after}")
     assert torch.allclose(result_before, result_after)
+
+
+@pytest.mark.parametrize(
+    "input_shape, dim, start, end",
+    (
+        ((1, 1, 1, 256), 3, 0, -1),
+        ((1, 256), 0, 0, -1),
+        ((1, 512), 0, 0, -1),
+        ((1, 512), 1, 0, 256),
+    ),
+)
+def test_slice2(device, input_shape, dim, start, end):
+    m = SliceModule2()
+    input_tensor = torch.randn(input_shape, dtype=torch.bfloat16)
+    result_before = m.forward(input_tensor, dim, start, end)
+    option = torch_ttnn.TorchTtnnOption(device=device)
+    
+    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
+    result_after = m.forward(input_tensor, dim, start, end)
+    option._out_fx_graphs[0].print_tabular()
+
+    # Check the graph has been rewritten and contains ttnn ops
+    if not (start == 0 and (end == -1 or end == input_shape[dim])):
+        nodes = list(option._out_fx_graphs[0].nodes)
+        assert [node.target for node in nodes].count(ttnn.slice) == 1
+    # Check inference result
+    print(f"torch_result_shape:{result_before.shape}\nttnn_result_shape:{result_after.shape}")
+    # print(f"result_before:{result_before}\nresult_after:{result_after}")
+    # assert torch.allclose(result_before, result_after)
--- torch_ttnn/passes/lowering/to_tt_pass.py
+++ torch_ttnn/passes/lowering/to_tt_pass.py
@@ -574,16 +574,38 @@ def ReplaceMoreTtManually(gm: torch.fx.GraphModule) -> torch.fx.GraphModule:
                 if start == 0 and end >= input_size[dim]:
                     return tensor
 
-                if len(input_size) != 4:
-                    return None
+                # This is for handling non 4D tensors
+                to_4d_shape = input_size
+                if len(input_size) == 1:
+                    dim += 3
+                    to_4d_shape = [1, 1, 1, input_size[0]]
+                elif len(input_size) == 2:
+                    to_4d_shape = [1, 1, input_size[0], input_size[1]]
+                    dim += 2
+                elif len(input_size) == 3:
+                    to_4d_shape = tensor, [1, input_size[0], input_size[1], input_size[2]]
+                    dim += 1
+                
+                slice_start = np.zeros(len(to_4d_shape), dtype=int)
+                slice_end = np.array(to_4d_shape)
 
-                slice_start = np.zeros(len(input_size), dtype=int)
-                slice_end = np.array(input_size)
+                # For negative end values
+                if end < 0:
+                    end = end + to_4d_shape[dim] + 1
 
                 slice_start[dim] = start
-                slice_end[dim] = min(end, input_size[dim])
+                slice_end[dim] = min(end, to_4d_shape[dim])
                 slice_end -= 1
 
+                # For handling non-4D slice inputs
+                if len(input_size) < 4:
+                    reshape_to_4d_node = g.call_function(ttnn.reshape, args=(tensor, to_4d_shape))
+                    to_layout = g.call_function(ttnn.to_layout, (reshape_to_4d_node, TtnnRowMajorLayout()))
+                    slice_node = g.call_function(ttnn.slice, (to_layout, [*slice_start], [*slice_end]))
+                    reshape_to_original_shape = node.meta["val"].size()
+                    reshape_to_original_node = g.call_function(ttnn.reshape, args=(slice_node, reshape_to_original_shape))
+                    return reshape_to_original_node
+                
                 return g.call_function(ttnn.slice, (tensor, [*slice_start], [*slice_end]))
 
             if node.target == torch.ops.aten.repeat.default:
