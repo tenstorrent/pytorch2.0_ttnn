f64bb00cddaaa8aa282bbd36cbc3f43dd083d3f7
Chen-Pang He <chen.pang.he@jdh8.org>
2024-09-18 15:17:02 +0000

    Test converting `aten.fmod.Scalar`

--- tests/lowering/eltwise/binary/test_fmod.py
+++ tests/lowering/eltwise/binary/test_fmod.py
@@ -14,6 +14,35 @@ class FmodModule(torch.nn.Module):
 
 
 @pytest.mark.parametrize(
+    "input_shape, mod",
+    (
+        ((32, 32), 7),
+        ((1, 32), 3),
+        ((16, 16), 5),
+        ((4,), 1),
+        ((1, 2, 3, 6, 4), 1),
+    ),
+)
+def test_fmod_scalar(device, input_shape, mod):
+    m = FmodModule()
+    input = torch.rand(input_shape, dtype=torch.bfloat16) * (mod * 3)
+    result_before = m.forward(input, mod)
+    # print(torch.export.export(m, args=(input, mod)))
+
+    option = torch_ttnn.TorchTtnnOption(device=device, gen_graphviz=True)
+    # The compilation is lazy, so we need to run forward once to trigger the compilation
+    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
+    result_after = m.forward(input, mod)
+    option._out_fx_graphs[0].print_tabular()
+
+    # Check the graph has be rewritten and contains ttnn ops
+    nodes = list(option._out_fx_graphs[0].nodes)
+    assert [node.target for node in nodes].count(ttnn.fmod) == 1
+    # Check inference result
+    assert torch.allclose(result_before, result_after)
+
+
+@pytest.mark.parametrize(
     "input_shapes",
     (
         ((32, 32), (32, 32)),
