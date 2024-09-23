d5969fd6f9d56a1206d918cc43ebb462399541e9
Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>
2024-09-23 16:52:49 -0700

    Update test_squeeze.py

--- tests/lowering/tensor_manipulation/test_squeeze.py
+++ tests/lowering/tensor_manipulation/test_squeeze.py
@@ -15,18 +15,18 @@ class SqueezeDimModule(torch.nn.Module):
 @pytest.mark.parametrize(
     "input_shapes, dim",
     (        
-        (1, 1, 24576), 0),
-        (1, 1, 480, 640), 1),
-        (1, 14, 1), -1),
-        #(1, 19), 0),
-        (1, 25, 1), -1),
-        (1, 256, 1), -1),
-        (1, 32, 16), 0),
-        (3, 1370, 1, 1, 1280), -2),
-        (3, 197, 1, 1, 1024), -2),
-        (3, 197, 1, 1, 768), -2),
-        (3, 50, 1, 1, 1024), -2),
-        (3, 50, 1, 1, 768), -2),
+        ((1, 1, 24576), 0),
+        ((1, 1, 480, 640), 1),
+        ((1, 14, 1), -1),
+        #((1, 19), 0),
+        ((1, 25, 1), -1),
+        ((1, 256, 1), -1),
+        ((1, 32, 16), 0),
+        ((3, 1370, 1, 1, 1280), -2),
+        ((3, 197, 1, 1, 1024), -2),
+        ((3, 197, 1, 1, 768), -2),
+        ((3, 50, 1, 1, 1024), -2),
+        ((3, 50, 1, 1, 768), -2),
     ),
 )
 def test_squeeze_dim(device, input_shapes, dim):
@@ -42,7 +42,7 @@ def test_squeeze_dim(device, input_shapes, dim):
 
     # Check the graph has be rewritten and contain ttnn ops
     nodes = list(option._out_fx_graphs[0].nodes)
-    assert [node.target for node in nodes].count(ttnn.reshape) == 1
+    assert [node.target for node in nodes].count(ttnn.squeeze) == 1
     # Check inference result
     print(f"torch result shape: {result_before.shape}\nttnn result shape: {result_after.shape}")
     assert torch.allclose(result_before, result_after)
