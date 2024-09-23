ac29e3c8e1a04bd2e315fbf6f761b273828b8522
Jerry Wu <jerry@skymizer.com>
2024-09-23 02:16:10 +0000

    Use xfail

--- tests/lowering/reduction/test_var.py
+++ tests/lowering/reduction/test_var.py
@@ -13,32 +13,56 @@ class VarModule(torch.nn.Module):
 
 
 @pytest.mark.parametrize(
-    "input_shape, dim, keepdim, correction, converted",
+    "input_shape, dim, keepdim, correction",
     [
-        ((32, 32), [1], True, 0, True),
-        ((16, 32, 32), [1], True, 0, True),
-        ((16, 32, 32), 1, True, 0, True),
-        ((16, 32, 32), [2], True, 0, True),
-        ((16, 32, 32), [1, 2], True, 0, True),
+        ((32, 32), [1], True, 0),
+        ((16, 32, 32), [1], True, 0),
+        ((16, 32, 32), 1, True, 0),
+        ((16, 32, 32), [2], True, 0),
+        ((16, 32, 32), [1, 2], True, 0),
         # TODO(#242): Not support correction != 0
-        ((32, 32), [1], True, 1, False),
+        pytest.param((32, 32), [1], True, 1, marks=pytest.mark.xfail(reason="Not support correction != 0 (#240)")),
         # TODO(#242): Unsupported reduction operation
-        ((32, 32), [], True, 0, False),
+        pytest.param((32, 32), [], True, 0, marks=pytest.mark.xfail(reason="Unsupported reduction operation (#240)")),
         # TODO(#240): keepdim = false is not supported
-        ((32, 32), [1], False, 0, False),
+        pytest.param(
+            (32, 32), [1], False, 0, marks=pytest.mark.xfail(reason="keepdim = false is not supported (#240)")
+        ),
         # TODO(#240): Not ssupport reduction on < rank - 2 dims
-        ((16, 32, 32), [0], True, 0, False),
-        ((32, 32, 32), [0, 1, 2], True, 0, False),
+        pytest.param(
+            (16, 32, 32),
+            [0],
+            True,
+            0,
+            marks=pytest.mark.xfail(reason="Not ssupport reduction on < rank - 2 dims (#240)"),
+        ),
+        pytest.param(
+            (32, 32, 32),
+            [0, 1, 2],
+            True,
+            0,
+            marks=pytest.mark.xfail(reason="Not ssupport reduction on < rank - 2 dims (#240)"),
+        ),
         # TODO(#240): Unexpected output shape (1, 1) instead of (1)
-        ((32,), [], True, 0, False),
+        pytest.param(
+            (32,), [], True, 0, marks=pytest.mark.xfail(reason="Unexpected output shape (1, 1) instead of (1) (#240)")
+        ),
         # TODO(#240): Need -inf padding value
-        ((1, 32), [], True, 0, False),
-        ((32, 1), [], True, 0, False),
+        pytest.param((1, 32), [], True, 0, marks=pytest.mark.xfail(reason="Need -inf padding value (#240)")),
+        pytest.param((32, 1), [], True, 0, marks=pytest.mark.xfail(reason="Need -inf padding value (#240)")),
         # TODO(#240): Output reshape inside generic reduction can't handle non-tile-aligned size
-        ((1, 32), [1], True, 0, False),
+        pytest.param(
+            (1, 32),
+            [1],
+            True,
+            0,
+            marks=pytest.mark.xfail(
+                reason="Output reshape inside generic reduction can't handle non-tile-aligned size (#240)"
+            ),
+        ),
     ],
 )
-def test_var(device, input_shape, dim, keepdim, correction, converted):
+def test_var(device, input_shape, dim, keepdim, correction):
     m = VarModule()
     input = torch.rand(input_shape, dtype=torch.bfloat16)
     result_before = m.forward(input, dim, keepdim, correction)
@@ -51,7 +75,7 @@ def test_var(device, input_shape, dim, keepdim, correction, converted):
 
     # Check the graph has be rewritten
     nodes = list(option._out_fx_graphs[0].nodes)
-    assert [node.target for node in nodes].count(ttnn.var) == (1 if converted else 0)
+    assert [node.target for node in nodes].count(ttnn.var) == 1
     # Check inference result
     assert result_before.shape == result_after.shape
     # Multiple float multiplications needs higher tolerance
