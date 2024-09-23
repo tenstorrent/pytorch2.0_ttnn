e985ae9d7b01cd2bfe9a4834a5afde18a638d95d
Jerry Wu <jerry@skymizer.com>
2024-09-23 02:08:57 +0000

    Use xfail

--- tests/lowering/reduction/test_prod.py
+++ tests/lowering/reduction/test_prod.py
@@ -13,26 +13,32 @@ class ProdDimIntModule(torch.nn.Module):
 
 
 @pytest.mark.parametrize(
-    "input_shape, dim, keep_dim, converted",
+    "input_shape, dim, keep_dim",
     [
-        ((1, 2, 32, 32), 3, True, True),
-        ((1, 1, 32, 32), 2, True, True),
-        ((1, 2, 32, 32), 1, True, True),
-        ((2, 1, 32, 32), 0, True, True),
-        ((2, 1, 32, 32), 0, True, True),
-        # TODO(#244): Unexpected output shape [1, 1, 2, 1]
-        ((1, 1, 2, 32, 32), -1, True, False),
-        # TODO(#244): Not support keep_dim = False
-        ((1, 2, 32, 32), 3, False, False),
-        # TODO(#244): dim >= -4 && dim <= 3 && "Dimension out of range (expected to be in range of [-4, 3]"
-        ((1, 1, 1, 32, 32), 4, True, False),
-        # TODO(#244): Need to pad with 1.0 instead of 0
-        ((1, 1, 32, 16), -1, True, False),
-        # TODO(#244): Input rank can't < 4
-        ((32, 32), 1, True, False),
+        ((1, 2, 32, 32), 3, True),
+        ((1, 1, 32, 32), 2, True),
+        ((1, 2, 32, 32), 1, True),
+        ((2, 1, 32, 32), 0, True),
+        ((2, 1, 32, 32), 0, True),
+        pytest.param(
+            (1, 1, 2, 32, 32), -1, True, marks=pytest.mark.xfail(reason="Unexpected output shape [1, 1, 2, 1] (#244)")
+        ),
+        pytest.param((1, 2, 32, 32), 3, False, marks=pytest.mark.xfail(reason="Not support keep_dim = False (#244)")),
+        pytest.param(
+            (1, 1, 1, 32, 32),
+            4,
+            True,
+            marks=pytest.mark.xfail(
+                reason='dim >= -4 && dim <= 3 && "Dimension out of range (expected to be in range of [-4, 3]" (#244)'
+            ),
+        ),
+        pytest.param(
+            (1, 1, 32, 16), -1, True, marks=pytest.mark.xfail(reason="Need to pad with 1.0 instead of 0 (#244)")
+        ),
+        pytest.param((32, 32), 1, True, marks=pytest.mark.xfail(reason="Input rank can't < 4")),
     ],
 )
-def test_prod_dim_int(device, input_shape, dim, keep_dim, converted):
+def test_prod_dim_int(device, input_shape, dim, keep_dim):
     m = ProdDimIntModule()
     input = torch.rand(input_shape, dtype=torch.bfloat16) + 0.5
     result_before = m.forward(input, dim, keep_dim)
@@ -45,7 +51,7 @@ def test_prod_dim_int(device, input_shape, dim, keep_dim, converted):
 
     # Check the graph has be rewritten
     nodes = list(option._out_fx_graphs[0].nodes)
-    assert [node.target for node in nodes].count(ttnn.prod) == (1 if converted else 0)
+    assert [node.target for node in nodes].count(ttnn.prod) == 1
     # Check inference result
     assert result_before.shape == result_after.shape
     # Give higher tolerance for product as it's not associative with float
