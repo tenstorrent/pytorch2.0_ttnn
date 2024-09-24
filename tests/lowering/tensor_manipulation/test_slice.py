7a2b2c916bc6fa8872f5abce9941f59d12c0df23
Kevin Wu <kevinwu@tenstorrent.com>
2024-09-24 22:44:41 +0000

    Update slice lowering to be compatible with new ttnn.slice API for tt-metal > v0.52.0-rc28

--- tests/lowering/tensor_manipulation/test_slice.py
+++ tests/lowering/tensor_manipulation/test_slice.py
@@ -4,23 +4,22 @@ import pytest
 import ttnn
 
 
+# Maps special integers to compatible values for aten.slice
+# aten.slice treats 9223372036854775807 (64-bit max int) as the last index
+def convert_int(val):
+    special_int_map = {-1: 9223372036854775807}
+    if (ret := special_int_map.get(val)) is not None:
+        return ret
+    else:
+        return val
+
+
 class SliceModule(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
     def forward(self, input, start, end):
         return input[:, :, start:end]
-
-
-class SliceModule2(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, input, dim, start, end):
-        # NOTE: aten.slice treats 9223372036854775807 (64-bit max int) as the last index
-        if end == -1:
-            end = 9223372036854775807
-        return torch.ops.aten.slice.Tensor(input, dim, start, end)
 
 
 @pytest.mark.parametrize(
@@ -49,6 +48,51 @@ def test_slice(device, input_shape, start, end):
     assert torch.equal(result_before, result_after)
 
 
+class AtenSliceModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input, dim, start, end):
+        return torch.ops.aten.slice.Tensor(input, dim, start, end)
+
+
+class AtenSliceAfterOpModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input, dim, start, end):
+        abs = torch.abs(input)
+        return torch.ops.aten.slice.Tensor(abs, dim, start, end)
+
+
+class AtenSliceBeforeOpModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input, dim, start, end):
+        slice = torch.ops.aten.slice.Tensor(input, dim, start, end)
+        return torch.abs(slice)
+
+
+class AtenSliceBetweenOpModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input, dim, start, end):
+        abs = torch.abs(input)
+        slice = torch.ops.aten.slice.Tensor(abs, dim, start, end)
+        return torch.abs(slice)
+
+
+@pytest.mark.parametrize(
+    "module",
+    [
+        AtenSliceModule(),
+        AtenSliceAfterOpModule(),
+        AtenSliceBeforeOpModule(),
+        AtenSliceBetweenOpModule(),
+    ],
+)
 @pytest.mark.parametrize(
     "input_shape, dim, start, end",
     (
@@ -58,10 +102,10 @@ def test_slice(device, input_shape, start, end):
         ((1, 512), 1, 0, 256),
     ),
 )
-def test_slice2(device, input_shape, dim, start, end):
-    m = SliceModule2()
+def test_aten_slice(device, input_shape, dim, start, end, module):
+    m = module
     input_tensor = torch.randn(input_shape, dtype=torch.bfloat16)
-    result_before = m.forward(input_tensor, dim, start, end)
+    result_before = m.forward(input_tensor, dim, start, convert_int(end))
     option = torch_ttnn.TorchTtnnOption(device=device)
 
     m = torch.compile(m, backend=torch_ttnn.backend, options=option)
@@ -69,9 +113,55 @@ def test_slice2(device, input_shape, dim, start, end):
     option._out_fx_graphs[0].print_tabular()
 
     # Check the graph has been rewritten and contains ttnn ops
+    # Ignore if no-op
     if not (start == 0 and (end == -1 or end == input_shape[dim])):
         nodes = list(option._out_fx_graphs[0].nodes)
         assert [node.target for node in nodes].count(ttnn.slice) == 1
     # Check inference result
     print(f"torch_result_shape:{result_before.shape}\nttnn_result_shape:{result_after.shape}")
+    assert torch.equal(result_before, result_after)
+
+
+class SliceEmbeddingModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input, dim, start, end, weights):
+        slice = torch.ops.aten.slice.Tensor(input, dim, start, end)
+        embedding = torch.ops.aten.embedding(weights, slice)
+        return torch.abs(embedding)
+
+
+@pytest.mark.parametrize(
+    "weights_shape",
+    [
+        (512, 1024),
+    ],
+)
+@pytest.mark.parametrize(
+    "input_shape, dim, start, end",
+    (
+        ((1, 256), 0, 0, -1),
+        ((1, 512), 0, 0, -1),
+        ((1, 512), 1, 0, 256),
+    ),
+)
+def test_reshape_slice_embedding(device, input_shape, dim, start, end, weights_shape):
+    m = SliceEmbeddingModule()
+    print(convert_int(end))
+    input = torch.randint(0, 16, input_shape)
+    weights = torch.rand(weights_shape, dtype=torch.bfloat16)
+    result_before = m.forward(input, dim, start, convert_int(end), weights)
+    option = torch_ttnn.TorchTtnnOption(device=device)
+
+    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
+    result_after = m.forward(input, dim, start, end, weights)
+    option._out_fx_graphs[0].print_tabular()
+
+    # Check the graph has been rewritten and contains ttnn ops
+    # Ignore if no-op
+    if not (start == 0 and (end == -1 or end == input_shape[dim])):
+        nodes = list(option._out_fx_graphs[0].nodes)
+        assert [node.target for node in nodes].count(ttnn.slice) == 1
+    # Check inference result
     assert torch.equal(result_before, result_after)
