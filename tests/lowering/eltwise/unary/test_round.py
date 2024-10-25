5f4143d3ff18ad2de06eeda317c219e05ce5451a
Chen-Pang He <chen.pang.he@jdh8.org>
2024-10-25 07:53:41 +0000

    Convert `torch.round` with various decimal places

--- tests/lowering/eltwise/unary/test_round.py
+++ tests/lowering/eltwise/unary/test_round.py
@@ -9,8 +9,8 @@ class RoundModule(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x):
-        return torch.round(x)
+    def forward(self, *args, **kwargs):
+        return torch.round(*args, **kwargs)
 
 
 @pytest.mark.skip_platform("grayskull")
@@ -24,7 +24,7 @@ class RoundModule(torch.nn.Module):
         (1066,),
     ),
 )
-def test_round(device, input_shape):
+def test_round_default(device, input_shape):
     m = RoundModule()
     input = torch.rand(input_shape, dtype=torch.bfloat16) * 20 - 10
     result_before = m.forward(input)
@@ -32,6 +32,31 @@ def test_round(device, input_shape):
     option.gen_graphviz = True
     m = torch.compile(m, backend=torch_ttnn.backend, options=option)
     result_after = m.forward(input)
+    option._out_fx_graphs[0].print_tabular()
+    nodes = list(option._out_fx_graphs[0].nodes)
+    assert [node.target for node in nodes].count(ttnn.round) == 1
+    assert_with_pcc(result_before, result_after, 0.99)
+
+
+@pytest.mark.skip_platform("grayskull")
+@pytest.mark.parametrize(
+    "input_shape, decimals",
+    (
+        ((1, 1, 1, 42), 1),
+        ((1, 1, 32, 1), 2),
+        ((4, 4), 3),
+        ((4, 32), 0),
+        ((1066,), -1),
+    ),
+)
+def test_round_decimals(device, input_shape, decimals):
+    m = RoundModule()
+    input = torch.rand(input_shape, dtype=torch.bfloat16) * 20 - 10
+    result_before = m.forward(input, decimals=decimals)
+    option = torch_ttnn.TorchTtnnOption(device=device)
+    option.gen_graphviz = True
+    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
+    result_after = m.forward(input, decimals=decimals)
     option._out_fx_graphs[0].print_tabular()
     nodes = list(option._out_fx_graphs[0].nodes)
     assert [node.target for node in nodes].count(ttnn.round) == 1
