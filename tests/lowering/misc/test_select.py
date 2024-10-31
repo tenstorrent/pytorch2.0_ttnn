08b155c5c66a408a1fe6ffc6109be103747cc444
Po-Sheng Chang <posheng_chang@skymizer.com>
2024-10-31 05:15:10 +0000

    Add more test parameter combinations in test_select.py

--- tests/lowering/misc/test_select.py
+++ tests/lowering/misc/test_select.py
@@ -10,18 +10,37 @@ class SelectModule(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, input):
-        return torch.select(input, 3, 0)
+    def forward(self, input, dim):
+        return torch.select(input, dim, 0)
 
 
 @pytest.mark.parametrize(
-    "input_shapes",
-    [(1, 32, 16, 3, 96)],
+    "input_shapes, dim",
+    [
+        ((3, 32, 16, 3, 96), 3),
+        ((3, 32, 16, 3, 96), 2),
+        ((3, 32, 16, 3, 96), 1),
+        ((3, 32, 16, 3, 96), 0),
+
+        ((3, 32, 16, 96), 1),
+        ((3, 32, 16, 96), 0),
+
+        ((3, 32, 16), 2),
+        ((3, 32, 16), 1),
+        ((3, 32, 16), 0),
+
+        ((3, 32), 0),
+
+        pytest.param((3, 32, 16, 3, 96), 4, marks=pytest.mark.xfail()),
+        pytest.param((3, 32, 16, 96), 3, marks=pytest.mark.xfail()),
+        pytest.param((3, 32, 16, 96), 2, marks=pytest.mark.xfail()),
+        pytest.param((3, 32), 1, marks=pytest.mark.xfail()),
+    ],
 )
-def test_select(device, input_shapes):
+def test_select(device, input_shapes, dim):
     m = SelectModule()
     inputs = torch.rand(input_shapes, dtype=torch.bfloat16)
-    result_before = m.forward(inputs)
+    result_before = m.forward(inputs, dim)
 
     option = torch_ttnn.TorchTtnnOption(device=device)
     option.gen_graphviz = True
@@ -29,7 +48,7 @@ def test_select(device, input_shapes):
     # The compilation is lazy, so we need to run forward once to trigger the compilation
     m = torch.compile(m, backend=torch_ttnn.backend, options=option)
 
-    result_after = m.forward(inputs)
+    result_after = m.forward(inputs, dim)
     option._out_fx_graphs[0].print_tabular()
 
     # Check the graph has be rewritten and contain ttnn ops
