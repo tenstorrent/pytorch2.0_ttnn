87a3a609ae5e513f380e23eb06a92d943f569c62
Po-Sheng Chang <posheng_chang@skymizer.com>
2024-11-01 00:42:08 +0000

    Add more tests in test_select.py
    * add index parameter to test more conditions
    * check if there is no aten.select instead of the lowering TTNN result

--- tests/lowering/misc/test_select.py
+++ tests/lowering/misc/test_select.py
@@ -10,33 +10,41 @@ class SelectModule(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, input, dim):
-        return torch.select(input, dim, 0)
+    def forward(self, input, dim, index):
+        return torch.select(input, dim, index)
 
 
 @pytest.mark.parametrize(
-    "input_shapes, dim",
+    "input_shapes, dim, index",
     [
-        ((3, 32, 16, 3, 96), 3),
-        ((3, 32, 16, 3, 96), 2),
-        ((3, 32, 16, 3, 96), 1),
-        ((3, 32, 16, 3, 96), 0),
-        ((3, 32, 16, 96), 1),
-        ((3, 32, 16, 96), 0),
-        ((3, 32, 16), 2),
-        ((3, 32, 16), 1),
-        ((3, 32, 16), 0),
-        ((3, 32), 0),
-        pytest.param((3, 32, 16, 3, 96), 4, marks=pytest.mark.xfail()),
-        pytest.param((3, 32, 16, 96), 3, marks=pytest.mark.xfail()),
-        pytest.param((3, 32, 16, 96), 2, marks=pytest.mark.xfail()),
-        pytest.param((3, 32), 1, marks=pytest.mark.xfail()),
+        ((3, 32, 16, 3, 96), 3, 0),
+        ((3, 32, 16, 3, 96), 3, 1),
+        ((3, 32, 16, 3, 96), 3, 2),
+        ((3, 32, 16, 3, 96), 2, 0),
+        ((3, 32, 16, 3, 96), 2, 1),
+        ((3, 32, 16, 3, 96), 2, 2),
+        ((3, 32, 16, 3, 96), 1, 0),
+        ((3, 32, 16, 3, 96), 1, 1),
+        ((3, 32, 16, 3, 96), 1, 2),
+        ((3, 32, 16, 3, 96), 0, 0),
+        ((3, 32, 16, 3, 96), 0, 1),
+        ((3, 32, 16, 3, 96), 0, 2),
+        ((3, 32, 16, 96), 1, 0),
+        ((3, 32, 16, 96), 0, 0),
+        ((3, 32, 16), 2, 0),
+        ((3, 32, 16), 1, 0),
+        ((3, 32, 16), 0, 0),
+        ((3, 32), 0, 0),
+        pytest.param((3, 32, 16, 3, 96), 4, 0, marks=pytest.mark.xfail()),
+        pytest.param((3, 32, 16, 96), 3, 0, marks=pytest.mark.xfail()),
+        pytest.param((3, 32, 16, 96), 2, 0, marks=pytest.mark.xfail()),
+        pytest.param((3, 32), 1, 0, marks=pytest.mark.xfail()),
     ],
 )
-def test_select(device, input_shapes, dim):
+def test_select(device, input_shapes, dim, index):
     m = SelectModule()
     inputs = torch.rand(input_shapes, dtype=torch.bfloat16)
-    result_before = m.forward(inputs, dim)
+    result_before = m.forward(inputs, dim, index)
 
     option = torch_ttnn.TorchTtnnOption(device=device)
     option.gen_graphviz = True
@@ -44,12 +52,12 @@ def test_select(device, input_shapes, dim):
     # The compilation is lazy, so we need to run forward once to trigger the compilation
     m = torch.compile(m, backend=torch_ttnn.backend, options=option)
 
-    result_after = m.forward(inputs, dim)
+    result_after = m.forward(inputs, dim, index)
     option._out_fx_graphs[0].print_tabular()
 
     # Check the graph has be rewritten and contain ttnn ops
     nodes = [node.target for node in option._out_fx_graphs[0].nodes]
-    assert nodes.count(ttnn.slice) == 1 and (nodes.count(ttnn.squeeze) == 1 or nodes.count(ttnn.reshape) == 1)
+    assert torch.ops.aten.select.int not in nodes
 
     # Check inference result
     assert_with_pcc(result_before, result_after, pcc=0.99)
