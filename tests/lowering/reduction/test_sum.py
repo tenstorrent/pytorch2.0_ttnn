e48da8dfe9f7216acdaf9e2c7a3a8971a42fb7e6
Chen-Pang He <chen.pang.he@jdh8.org>
2024-11-09 08:16:01 +0000

    Test more cases from docs/operations/aten.sum.dim_IntList.md

--- tests/lowering/reduction/test_sum.py
+++ tests/lowering/reduction/test_sum.py
@@ -16,7 +16,19 @@ class SumDimModule(torch.nn.Module):
 
 @pytest.mark.parametrize(
     "input_shape, dim",
-    [((1, 32, 32), (-1, -2))],
+    (
+        ((1, 32, 32), (-1, -2)),
+        ((1, 1, 768), (0, 1)),
+        ((1, 1000), (0,)),
+        ((1, 1024, 256), (0, 1)),
+        ((1, 1024, 7, 7), (2, 3)),
+        ((1, 12, 16), (1,)),
+        ((1, 12, 16), (2,)),
+        ((2, 512), (1,)),
+        ((2, 7, 512), (0,)),
+        ((50, 768), (0,)),
+        ((768, 196), (0,)),
+    ),
 )
 def test_sum_dim(device, input_shape, dim):
     m = SumDimModule()
@@ -32,6 +44,7 @@ def test_sum_dim(device, input_shape, dim):
 
     # Check the graph has been rewritten and contains ttnn ops
     nodes = list(option._out_fx_graphs[0].nodes)
-    assert [node.target for node in nodes].count(ttnn.sum) > 0
+    trivial = all(input_shape[n] == 1 for n in dim)
+    assert [node.target for node in nodes].count(ttnn.sum) >= (not trivial)
     # Check inference result
     assert_with_pcc(result_before, result_after)
