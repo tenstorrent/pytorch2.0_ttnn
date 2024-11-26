b848438e682dc38288925f56fd2c095092f1437e
Chen-Pang He <chen.pang.he@jdh8.org>
2024-11-26 19:49:27 +0000

    Simplify testing logic for converting `aten.rsub.Scalar`

--- tests/lowering/eltwise/binary/test_sub.py
+++ tests/lowering/eltwise/binary/test_sub.py
@@ -101,18 +101,18 @@ def test_rsub(device, input_shapes):
 
 
 @pytest.mark.parametrize(
-    "input_shapes",
-    [[(4, 4)], [(32, 32)]],
+    "input_shape",
+    ((4, 4), (32, 32)),
 )
-def test_rsub_scalar(device, input_shapes):
+def test_rsub_scalar(device, input_shape):
     m = RSubScalarModule()
-    inputs = [torch.rand(shape, dtype=torch.bfloat16) for shape in input_shapes]
-    result_before = m.forward(*inputs, 5)
+    input = torch.rand(input_shape, dtype=torch.bfloat16)
+    result_before = m.forward(input, 5)
     option = torch_ttnn.TorchTtnnOption(device=device)
     option.gen_graphviz = True
     # The compilation is lazy, so we need to run forward once to trigger the compilation
     m = torch.compile(m, backend=torch_ttnn.backend, options=option)
-    result_after = m.forward(*inputs, 5)
+    result_after = m.forward(input, 5)
     option._out_fx_graphs[0].print_tabular()
 
     # Check the graph has be rewritten and contain ttnn ops
@@ -124,7 +124,7 @@ def test_rsub_scalar(device, input_shapes):
     # Intermediate node meta check if preserved
     for node in nodes:
         if node.target == ttnn.full:
-            assert node.meta["val"].size() == input_shapes[0]
+            assert node.meta["val"].size() == input_shape
 
     # Check inference result
     assert_with_pcc(result_before, result_after, 0.998)
