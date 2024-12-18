c12ab3e4bfd4769b77e8c49e0757f105e70d0bae
GitHub Action <action@github.com>
2024-12-18 03:41:14 +0000

    [auto] Pre-commit run on all files

--- torch_ttnn/passes/lowering/to_tt_pass.py
+++ torch_ttnn/passes/lowering/to_tt_pass.py
@@ -784,7 +784,7 @@ def ReplaceMoreTtManually(gm: torch.fx.GraphModule, use_less_ttnn_op_types: bool
                 full_pad = [(0, 0)] * (rank - len(pad))
                 # The order of pad from pytorch is reversed
                 full_pad += [(pad[i], pad[i + 1]) for i in range(0, len(pad), 2)][::-1]
-                
+
                 if (
                     rank < 2
                     or input_shape[-1] % ttnn.TILE_SIZE != 0
