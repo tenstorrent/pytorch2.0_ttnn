3a90bebcf3af47e7d40cfbb5741a87813ed59d13
swimdi <swimdi@skymizer.com>
2024-12-20 14:58:36 +0800

    Update guard and model tests because some case now is pass

--- torch_ttnn/passes/lowering/to_tt_guard.py
+++ torch_ttnn/passes/lowering/to_tt_guard.py
@@ -2,110 +2,8 @@ from functools import partial
 from .to_tt_guard_autogen import *
 
 ############################################################
-# EXTRA BLOCKLIST OF retinanet_resnet50_fpn*
-############################################################
-
-# retinanet_resnet50_fpn
-# RuntimeError: Missing conversion from torch.dtype: torch.int32 to Ttnn dtype.
-# => comment ttnn_dtype = torch_dtype_to_ttnn_dtype(arg_metadata.dtype), it is not used now
-
-# RuntimeError: _unsafe_index found unexpected index type BFloat16
-# ... => aten::sub.Tensor => aten::div.Tensor => unsqueeze => unsafe_index
-# it is too long to block, so just wait the to_tt_pass fix
-
-# TODO: not pass yet
-
-############################################################
-# EXTRA BLOCKLIST OF ghostnetv2_100.in1k*
-############################################################
-
-# see issue #390
-# ghostnetv2_100.in1k
-# RuntimeError: _unsafe_index found unexpected index type BFloat16
-# arange => add => mul => to_copy => unsqueeze => unsafe_index
-aten_add_Tensor_blocklist = []
-aten_add_Tensor_blocklist += [
-    ["Tensor<[56]> self = ?", "Tensor other = 0.0"],
-    ["Tensor<[28]> self = ?", "Tensor other = 0.0"],
-    ["Tensor<[14]> self = ?", "Tensor other = 0.0"],
-    ["Tensor<[7]> self = ?", "Tensor other = 0.0"],
-]
-
-aten_mul_Tensor_blocklist = []
-aten_mul_Tensor_blocklist += [
-    ["Tensor<[56]> self = ?", "Tensor other = 0.5"],
-    ["Tensor<[28]> self = ?", "Tensor other = 0.5"],
-    ["Tensor<[14]> self = ?", "Tensor other = 0.5"],
-    ["Tensor<[7]> self = ?", "Tensor other = 0.42857142857142855"],
-]
-
-# ghostnetv2_100.in1k-train
-# TODO:
-# RuntimeError: Index put requires the source and destination dtypes match,
-# got Float for the destination and BFloat16 for the source.
-# gm.code:
-# new_zeros_default_1 = torch.ops.aten.new_zeros.default(
-#     mul_tensor_2, [1, 960, 3, 3], dtype=torch.float32, layout=torch.strided, device=device(type="cpu")
-# )
-# _unsafe_index_put_default = torch.ops.aten._unsafe_index_put.default(
-#     new_zeros_default_1, [None, None, unsqueeze_13, _to_copy_292], ttnn_to_torch_7, True
-# )
-# new_zeros_default_1 is float32 type and ttnn_to_torch_7 is bf16 type
-# need a pass to equal _unsafe_index_put_default's input type
-
-############################################################
-# EXTRA BLOCKLIST OF hrnet_w18.ms_aug_in1k*
-############################################################
-# see issue #363
-# self = <OpOverload(op='aten.convolution', overload='default')>
-# args = (TorchTensor([[[[[6.6797e-01, 1.5703e+00, 1.1562e+00,  ..., 5.8203e-01,
-#                  3.3936e-02, 1.1953e+00],
-#     ...-03,  1.2360e-03, -4.0527e-02]]]], dtype=torch.bfloat16,
-#        requires_grad=True), None, [1, 1], [1, 1], [1, 1], ...)
-# kwargs = {}
-#     def __call__(self, *args, **kwargs):
-# >       return self._op(*args, **(kwargs or {}))
-# RuntimeError: Expected 4-dimensional input for 4-dimensional weight [18, 18, 3, 3], but got 5-dimensional input of size [1, 18, 18, 56, 56] instead
-# gm.code:
-# add_tensor = torch.ops.aten.add.Tensor(arange_default, 0.0);  arange_default = None
-# mul_tensor = torch.ops.aten.mul.Tensor(add_tensor, 0.5)
-# _to_copy_default_1 = torch.ops.aten._to_copy.default(mul_tensor, dtype = torch.int64);  mul_tensor = None
-# unsqueeze_default = torch.ops.aten.unsqueeze.default(_to_copy_default_1, -1)
-# _unsafe_index_tensor = torch.ops.aten._unsafe_index.Tensor(getitem_33, [None, None, unsqueeze_default, _to_copy_default_1]);  getitem_33 = None
-# ttnn_from_torch_33 = ttnn_decorators_ttnn_from_torch(_unsafe_index_tensor, layout = ttnn_TILE_LAYOUT, dtype = ttnn_bfloat16, device = ttnn_Specified_Device);  _unsafe_index_tensor = None
-# ttnn_add_12 = ttnn_decorators_ttnn_add(ttnn_relu_23, ttnn_from_torch_33);  ttnn_from_torch_33 = None
-# ttnn_relu_32 = ttnn_decorators_ttnn_relu(ttnn_add_12);  ttnn_add_12 = None
-# ttnn_to_torch_33 = ttnn_decorators_ttnn_to_torch(ttnn_relu_23);  ttnn_relu_23 = None
-# convolution_default_34 = torch.ops.aten.convolution.default(ttnn_to_torch_33, arg102_1, None, [2, 2], [1, 1], [1, 1], False, [0, 0], 1);  ttnn_to_torch_33 = arg102_1 = None
-# Don't know why mul cause failed
-
-aten_mul_Tensor_blocklist += [
-    ["Tensor<[56]> self = ?", "Tensor other = 0.5"],
-    ["Tensor<[56]> self = ?", "Tensor other = 0.25"],
-    ["Tensor<[28]> self = ?", "Tensor other = 0.5"],
-    ["Tensor<[56]> self = ?", "Tensor other = 0.125"],
-    ["Tensor<[28]> self = ?", "Tensor other = 0.25"],
-    ["Tensor<[14]> self = ?", "Tensor other = 0.5"],
-]
-
-# hrnet_w18.ms_aug_in1k train
-#     def __call__(self, *args, **kwargs):
-# >       return self._op(*args, **(kwargs or {}))
-# RuntimeError: Index put requires the source and destination dtypes match, got Float for the destination and BFloat16 for the source.
-
-############################################################
 # EXTRA BLOCKLIST OF Whisper
 ############################################################
-# embedding
-# RuntimeError: Expected tensor for argument #1 'indices'
-# to have one of the following scalar types: Long, Int; but got CPUB...
-# ones = torch.ops.aten.ones.default([1, 1], dtype = torch.int64, device = device(type='cpu'), pin_memory = False)
-# mul = torch.ops.aten.mul.Tensor(ones, 50258);  ones = None
-# view_192 = torch.ops.aten.view.default(mul, [-1, 1]);  mul = None
-# embedding = torch.ops.aten.embedding.default(arg188_1, view_192, 50257);  arg188_1 = view_192 = None
-
-aten_mul_Tensor_blocklist += [["Tensor<[1, 1]> self = ?", "Tensor other = 50258"]]
-
 # torch._dynamo.exc.BackendCompilerFailed: backend='ttnn_backend' raised:
 # RuntimeError: aten::clone() Expected a value of type 'Tensor' for argument 'self' but instead found type 'SymInt'.
 # Position: 0
@@ -117,13 +15,12 @@ aten_mul_Tensor_blocklist += [["Tensor<[1, 1]> self = ?", "Tensor other = 50258"
 ############################################################
 # EXTRA BLOCKLIST OF GPTNeo
 ############################################################
-# RuntimeError: TT_FATAL @ xxx/ttnn/cpp/ttnn/operations/data_movement/slice/slice.cpp:110:
-# modified_ends[idx] >= modified_begins[idx]
-
-aten_select_int_blocklist = [["Tensor<[1, 45]> self = ?", "int dim = 1", "int index = -1"]]
-
-# ERROR tests/models/gpt_neo/test_gpt_neo.py::test_gpt_neo[eval], see issue #503
-# RuntimeError: probability tensor contains either `inf`, `nan` or element < 0
+# torch._dynamo.exc.BackendCompilerFailed: backend='ttnn_backend' raised:
+# RuntimeError: aten::clone() Expected a value of type 'Tensor' for argument 'self' but instead found type 'SymInt'.
+# Position: 0
+# Value: s0
+# Declaration: aten::clone(Tensor self, *, MemoryFormat? memory_format=None) -> Tensor
+# Cast error details: Unable to cast s0 to Tensor
 # TODO: not pass yet
 
 ############################################################
@@ -144,20 +41,8 @@ aten_gt_Scalar_blocklist = [["Tensor<[]> self = ?", "number other = 0"]]
 # TODO: not pass yet
 
 ############################################################
-# EXTRA BLOCKLIST OF GLPN-KITTI
-############################################################
-# index out of bound, see issue #420
-# clamp => unsqueeze => unsafe_index
-aten_unsqueeze_default_blocklist = [["Tensor<[240]> self = ?", "int dim = 1"]]
-
-# IndexError: shape mismatch: indexing tensors could not be broadcast together with shapes [1, 1, 240], [1, 320]
-# guess is issue #390
-# TODO: not pass yet
-
-############################################################
 # EXTRA BLOCKLIST OF OPT
 ############################################################
-
 # ttnn.from_torch(torch.tensor(-3.3895e+38)) => ttnn.maximum
 # ttnn.from_torch not support scalar
 # RuntimeError: TT_FATAL @ tensor/types.cpp:209: normalized_index >= 0 and normalized_index < rank
@@ -191,75 +76,21 @@ aten_maximum_default_blocklist += [["Tensor<[1, 16, 59, 59]> self = ?", "Tensor 
 aten_cumsum_default_blocklist = [["Tensor<[1, 23, 40]> self = ?", "int dim = 1", "Optional[int] dtype = torch.float32"]]
 
 ############################################################
-# EXTRA BLOCKLIST OF ssd300_vgg16
-############################################################
-# IndexError: index 480 is out of bounds for dimension 0 with size 480, see issue #420
-# add => multiply => subtract => unsqueeze
-aten_add_Tensor_blocklist += [["Tensor<[300]> self = ?", "Tensor other = 0.5"]]
-aten_mul_Tensor_blocklist += [["Tensor<[300]> self = ?", "Tensor other = 1.6"]]
-aten_unsqueeze_default_blocklist += [["Tensor<[300]> self = ?", "int dim = 1"]]
-# RuntimeError: expected scalar type BFloat16 but found Float
-# convolution_default_5 weight dtype is float32, this is
-# because RuntimeError: "nms_kernel" not implemented for 'BFloat16' so cannot model.to(torch.bfloat16)
-# TODO: not pass yet
-
-############################################################
-# EXTRA BLOCKLIST OF ssdlite320_mobilenet_v3_large
-############################################################
-# IndexError: IndexError: index 640 is out of bounds for dimension 1 with size 640, see issue #420
-# add => multiply => subtract => unsqueeze
-aten_add_Tensor_blocklist += [["Tensor<[320]> self = ?", "Tensor other = 0.5"]]
-aten_mul_Tensor_blocklist += [
-    ["Tensor<[320]> self = ?", "Tensor other = 1.5"],
-    ["Tensor<[320]> self = ?", "Tensor other = 2.0"],
-]
-aten_unsqueeze_default_blocklist += [["Tensor<[320]> self = ?", "int dim = 1"]]
-
-# RuntimeError: expected scalar type BFloat16 but found Float
-# convolution_default_66 weight dtype is float32, this is
-# because RuntimeError: "nms_kernel" not implemented for 'BFloat16' so cannot model.to(torch.bfloat16)
-# TODO: not pass yet
-
-############################################################
-# EXTRA BLOCKLIST OF MobileNetSSD
-############################################################
-# IndexError: index 320 is out of bounds for dimension 0 with size 320, see issue #420
-# add => multiply => subtract => unsqueeze
-aten_add_Tensor_blocklist += [["Tensor<[320]> self = ?", "Tensor other = 0.5"]]
-aten_mul_Tensor_blocklist += [["Tensor<[320]> self = ?", "Tensor other = 1.0"]]
-aten_unsqueeze_default_blocklist += [["Tensor<[320]> self = ?", "int dim = 1"]]
-# RuntimeError: expected scalar type BFloat16 but found Float
-# convolution_default_66 weight dtype is float32, this is
-# because RuntimeError: "nms_kernel" not implemented for 'BFloat16' so cannot model.to(torch.bfloat16)
-# TODO: not pass yet
-
-############################################################
 # EXTRA BLOCKLIST OF retinanet_resnet50_fpn
 ############################################################
-# IndexError: index 480 is out of bounds for dimension 0 with size 480, see issue #420
-aten_add_Tensor_blocklist += [["Tensor<[800]> self = ?", "Tensor other = 0.5"]]
-aten_mul_Tensor_blocklist += [["Tensor<[800]> self = ?", "Tensor other = 0.6"]]
-aten_unsqueeze_default_blocklist += [["Tensor<[800]> self = ?", "int dim = 1"]]
-# RuntimeError: expected scalar type BFloat16 but found Float
-# convolution_default weight dtype is float32, this is
-# because RuntimeError: "nms_kernel" not implemented for 'BFloat16' so cannot model.to(torch.bfloat16)
+# Statically allocated circular buffers on core range [(x=0,y=0) - (x=0,y=0)] grow to 3580704 B which is beyond max L1 size of 1499136 B
 # TODO: not pass yet
 
 
 ############################################################
 # EXTRA BLOCKLIST OF retinanet_resnet50_fpn_v2
 ############################################################
-# RuntimeError: expected scalar type BFloat16 but found Float
-# convolution_default weight dtype is float32, this is
-# because RuntimeError: "nms_kernel" not implemented for 'BFloat16' so cannot model.to(torch.bfloat16)
+# Statically allocated circular buffers on core range [(x=0,y=0) - (x=0,y=0)] grow to 3580704 B which is beyond max L1 size of 1499136 B
 # TODO: not pass yet
 
 ############################################################
 
-GUARD[torch.ops.aten.add.Tensor] = partial(guard_aten, aten_add_Tensor_blocklist)
-GUARD[torch.ops.aten.select.int] = partial(guard_aten, aten_select_int_blocklist)
 GUARD[torch.ops.aten.gt.Scalar] = partial(guard_aten, aten_gt_Scalar_blocklist)
-GUARD[torch.ops.aten.unsqueeze.default] = partial(guard_aten, aten_unsqueeze_default_blocklist)
 GUARD[torch.ops.aten.cumsum.default] = partial(guard_aten, aten_cumsum_default_blocklist)
 
 
