57580ba97e7f96c43f8ce815ec9db0fd1a85c3f7
swimdi <swimdi@skymizer.com>
2025-02-03 22:59:32 +0800

    Use scm version

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
 
+
+def version_scheme(version):
+    with open("VERSION") as f:
+        version = f.read().strip()
+    return version
+
+
 setup(
     name="torch-ttnn",
-    version="0.1.0",
     description="PyTorch dynamo backend for Tenstorrent TT-NN framework",
     url="https://github.com/tenstorrent/pytorch2.0_ttnn",
+    use_scm_version={
+        "version_scheme": version_scheme,
+        "local_scheme": "node-and-date",
+    },
     packages=["torch_ttnn"],
 )
