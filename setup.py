a3f5d62d9cf15907bd10405ecd8573e66197cf72
swimdi <swimdi@skymizer.com>
2025-02-03 20:52:08 +0800

    Correct install dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(
     name="torch-ttnn",
     version="0.1.0",
     description="PyTorch dynamo backend for Tenstorrent TT-NN framework",
-    license="MIT",
     url="https://github.com/tenstorrent/pytorch2.0_ttnn",
     packages=["torch_ttnn"],
-    install_requires=[],
 )
