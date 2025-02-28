d5e5dca7e07640fa09814ba6330dfe3700deaf93
Kevin Wu <kevinwu@tenstorrent.com>
2025-02-28 00:09:44 +0000

    Integrate custom TTNN extension for Pytorch

--- torch_ttnn/passes/lowering/add_data_move_pass.py
+++ torch_ttnn/passes/lowering/add_data_move_pass.py
@@ -17,6 +17,7 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Union, Type, Literal
 from operator import getitem
+from torch_ttnn.cpp_extension.custom_device_mode import ttnn_module
 
 from torch.fx.passes.infra.pass_base import PassBase, PassResult
 from . import target_wrappers
@@ -439,8 +440,11 @@ class NodeInputAligner:
                 kwargs["layout"] = spec.layout()
             if spec.dtype is not None:
                 kwargs["dtype"] = spec.dtype()
-            return self.graph.call_function(ttnn.from_torch, (spec.input_node,), kwargs)
-
+            # g.get_attr(spec.)
+            if "val" in spec.input_node.meta and hasattr(spec.input_node.meta["val"], "device") and str(spec.input_node.meta["val"].device) == "ttnn:0":
+                return self.graph.call_function(ttnn_module.get_ttnn_tensor, (spec.input_node,), {})
+            else:
+                return self.graph.call_function(ttnn.from_torch, (spec.input_node,), kwargs)
         elif isinstance(spec, self.AlignSpecToTorch):
             if spec.input_node.meta.get("is_sharded"):
                 batch_dimension = 0
