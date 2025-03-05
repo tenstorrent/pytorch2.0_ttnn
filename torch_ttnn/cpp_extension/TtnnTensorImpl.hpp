974bccdc88a4ca98b59ed39bd3745c4d51abf2d5
Kevin Wu <kevinwu@tenstorrent.com>
2025-03-05 21:43:57 +0000

    Separate TtnnTensorImpl to cpp

--- torch_ttnn/cpp_extension/TtnnTensorImpl.hpp
+++ torch_ttnn/cpp_extension/TtnnTensorImpl.hpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+#include "c10/core/TensorImpl.h"
+#include "ATen/core/Tensor.h"
 #include "extension_utils.hpp"
 #include "ttnn/tensor/tensor.hpp"
 
@@ -16,47 +18,22 @@ struct TtnnTensorImpl : public TensorImpl {
         const caffe2::TypeMeta data_type,
         c10::Device device,
         ttnn::Tensor& ttnn_tensor,
-        c10::intrusive_ptr<c10::StorageImpl> storage) :
-        TensorImpl(key_set, data_type, device),
-        ttnn_tensor_(ttnn_tensor),
-        ttnn_tensor_string_(ttnn_tensor.write_to_string()) {
-        storage_ = std::move(storage);
-        auto view = ttnn_tensor_.get_logical_shape().view();
-        std::vector<int64_t> view_int64;
-        std::copy(view.begin(), view.end(), std::back_inserter(view_int64));
-        IntArrayRef int_array_ref(&(*view_int64.begin()), &(*view_int64.end()));
-        sizes_and_strides_.set_sizes(int_array_ref);
-    }
+        c10::intrusive_ptr<c10::StorageImpl> storage);
 
     TtnnTensorImpl(
         at::DispatchKeySet key_set,
         const caffe2::TypeMeta data_type,
         c10::Device device,
         const ttnn::Tensor& ttnn_tensor,
-        const Storage& storage) :
-        TensorImpl(key_set, data_type, device),
-        ttnn_tensor_(ttnn_tensor),
-        ttnn_tensor_string_(ttnn_tensor.write_to_string()) {
-        storage_ = std::move(storage);
-        auto view = ttnn_tensor_.get_logical_shape().view();
-        std::vector<int64_t> view_int64;
-        std::copy(view.begin(), view.end(), std::back_inserter(view_int64));
-        IntArrayRef int_array_ref(&(*view_int64.begin()), &(*view_int64.end()));
-        sizes_and_strides_.set_sizes(int_array_ref);
-    }
+        const c10::Storage& storage);
 
-    void set_sizes_and_strides(const IntArrayRef& int_array_ref) { sizes_and_strides_.set_sizes(int_array_ref); }
+    void set_sizes_and_strides(const IntArrayRef& int_array_ref);
 
-    void set_sizes_and_strides_as(const at::Tensor& the_template) {
-        sizes_and_strides_.set_sizes(the_template.sizes());
-    }
+    void set_sizes_and_strides_as(const at::Tensor& the_template);
 
-    ttnn::Tensor get_ttnn_tensor() {
-        LOGGING("");
-        return ttnn_tensor_;
-    }
+    ttnn::Tensor get_ttnn_tensor();
 
-    void set_ttnn_tensor(const ttnn::Tensor& tensor) { ttnn_tensor_ = tensor; }
+    void set_ttnn_tensor(const ttnn::Tensor& tensor);
 
     /**
      * Return a TensorImpl that is a shallow-copy of this TensorImpl.
@@ -65,16 +42,7 @@ struct TtnnTensorImpl : public TensorImpl {
      * see NOTE [ TensorImpl Shallow-Copying ].
      */
     c10::intrusive_ptr<TensorImpl> shallow_copy_and_detach(
-        const c10::VariableVersion& version_counter, bool allow_tensor_metadata_change) const override {
-        auto impl = c10::make_intrusive<TtnnTensorImpl>(key_set(), dtype(), device(), ttnn_tensor_, storage_);
-        copy_tensor_metadata(
-            /*src_opaque_impl=*/this,
-            /*dest_opaque_impl=*/impl.get(),
-            /*version_counter=*/version_counter,
-            /*allow_tensor_metadata_change=*/allow_tensor_metadata_change);
-        impl->refresh_numel();
-        return impl;
-    }
+        const c10::VariableVersion& version_counter, bool allow_tensor_metadata_change) const override;
 
     /**
      * Return a TensorImpl that is a shallow-copy of this TensorImpl.
@@ -83,16 +51,7 @@ struct TtnnTensorImpl : public TensorImpl {
      * see NOTE [ TensorImpl Shallow-Copying ].
      */
     c10::intrusive_ptr<TensorImpl> shallow_copy_and_detach(
-        c10::VariableVersion&& version_counter, bool allow_tensor_metadata_change) const override {
-        auto impl = c10::make_intrusive<TtnnTensorImpl>(key_set(), dtype(), device(), ttnn_tensor_, storage_);
-        copy_tensor_metadata(
-            /*src_opaque_impl=*/this,
-            /*dest_opaque_impl=*/impl.get(),
-            /*version_counter=*/std::move(version_counter),
-            /*allow_tensor_metadata_change=*/allow_tensor_metadata_change);
-        impl->refresh_numel();
-        return impl;
-    }
+        c10::VariableVersion&& version_counter, bool allow_tensor_metadata_change) const override;
 
     /**
      * Shallow-copies data from another TensorImpl into this TensorImpl.
@@ -100,16 +59,7 @@ struct TtnnTensorImpl : public TensorImpl {
      * For why this function doesn't check this TensorImpl's
      * `allow_tensor_metadata_change_`, see NOTE [ TensorImpl Shallow-Copying ].
      */
-    void shallow_copy_from(const c10::intrusive_ptr<TensorImpl>& impl) override {
-        AT_ASSERT(has_compatible_shallow_copy_type(impl->key_set()));
-        auto ttnn_impl = static_cast<const TtnnTensorImpl*>(impl.get());
-        copy_tensor_metadata(
-            /*src_impl=*/ttnn_impl,
-            /*dest_impl=*/this,
-            /*version_counter=*/version_counter(),
-            /*allow_tensor_metadata_change=*/allow_tensor_metadata_change());
-        refresh_numel();
-    }
+    void shallow_copy_from(const c10::intrusive_ptr<TensorImpl>& impl) override;
 
 private:
     ttnn::Tensor ttnn_tensor_;
