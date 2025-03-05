2c467339546ae4947a37770023ec57ffb436c4b9
Kevin Wu <kevinwu@tenstorrent.com>
2025-03-05 22:28:20 +0000

    Split cpp extension tests

---
+++ tests/cpp_extension/test_bert_cpp_extension.py
@@ -0,0 +1,77 @@
+import torch
+import torch_ttnn
+from torch_ttnn.cpp_extension.custom_device_mode import ttnn_module
+import time
+
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+
+
+def test_bert_with_cpp_extension(device):
+    model_name = "phiyodr/bert-large-finetuned-squad2"
+    tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left", torch_dtype=torch.bfloat16)
+    m = AutoModelForQuestionAnswering.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+    context = 'Johann Joachim Winckelmann was a German art historian and archaeologist. He was a pioneering Hellenist who first articulated the difference between Greek, Greco-Roman and Roman art. "The prophet and founding hero of modern archaeology", Winckelmann was one of the founders of scientific archaeology and first applied the categories of style on a large, systematic basis to the history of art. '
+    question = "What discipline did Winkelmann create?"
+    inputs = tokenizer.encode_plus(
+        question,
+        context,
+        add_special_tokens=True,
+        return_tensors="pt",
+        max_length=256,
+        padding="max_length",
+        truncation=True,
+    )
+
+    option = torch_ttnn.TorchTtnnOption(
+        device=device,
+        gen_graphviz=False,
+        run_mem_analysis=False,
+        metrics_path=model_name,
+        verbose=True,
+    )
+
+    # custom device
+    torch.utils.rename_privateuse1_backend("ttnn")
+    ttnn_device = ttnn_module.custom_device_from_ttnn(device)
+
+    # clone input_ids on cpu since this the data transfer is somehow inplace?
+    input_ids = inputs.input_ids.clone()
+
+    # Helper function to decode output to human-readable text
+    def decode_output(outputs):
+        response_start = torch.argmax(outputs.start_logits)
+        response_end = torch.argmax(outputs.end_logits) + 1
+        response_tokens = input_ids[0, response_start:response_end]
+        return tokenizer.decode(response_tokens)
+
+    # comment out these to disable cpp extension
+    start_to = time.perf_counter() * 1000
+    inputs = inputs.to(ttnn_device)
+    # modules are inplace, tensors are not
+    m.to(ttnn_device)
+    end_to = time.perf_counter() * 1000
+    print(f"to: {end_to - start_to} (ms)")
+
+    model = torch.compile(m, backend=torch_ttnn.backend, options=option)
+
+    for idx in range(5):
+        start = time.perf_counter() * 1000
+        # Don't need to reset options if inputs don't change because of cache
+        outputs = model(**inputs)
+        end = time.perf_counter() * 1000
+        run_time = end - start
+        print(f"iter {idx}: {run_time} (ms)")
+
+    print("finished:")
+    print(outputs)
+    answer = decode_output(outputs)
+
+    print(
+        f"""
+    model_name: {model_name}
+    input:
+        context: {context}
+        question: {question}
+    answer: {answer}
+    """
+    )
