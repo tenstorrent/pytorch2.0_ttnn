bc90cbef5148dd2622d88d31da41c2db6fb65aa7
Kevin Wu <kevinwu@tenstorrent.com>
2025-03-07 18:56:31 +0000

    Inline some helper functions

--- torch_ttnn/cpp_extension/extension_utils.hpp
+++ torch_ttnn/cpp_extension/extension_utils.hpp
@@ -31,7 +31,7 @@ void vector_compare(const std::vector<T>& first, const std::vector<T>& second) {
     }
 }
 
-ttnn::DataType dtype_torch_to_ttnn(at::ScalarType dtype) {
+inline ttnn::DataType dtype_torch_to_ttnn(at::ScalarType dtype) {
     switch (dtype) {
         case at::ScalarType::Int:
         case at::ScalarType::Long: return ttnn::DataType::UINT32;
@@ -39,3 +39,13 @@ ttnn::DataType dtype_torch_to_ttnn(at::ScalarType dtype) {
         default: return ttnn::DataType::BFLOAT16;
     }
 }
+
+inline void convert_vector_from_uint32_to_int(std::vector<int>& dst_vector, const std::vector<uint32_t>& src_vector) {
+    std::transform(
+        src_vector.begin(),
+        src_vector.end(),
+        std::back_inserter(dst_vector),
+        [](const uint32_t value) { return static_cast<int>(value); });
+}
+
+#define GET_OPTIONAL_OR_ASSERT(optional) ({TORCH_CHECK(optional.has_value()); optional.value();})
