64a3e42c94c1d5f78a4b2423f428067218755a1f
Kevin Wu <kevinwu@tenstorrent.com>
2025-03-10 14:45:06 +0000

    Major refactoring

---
+++ torch_ttnn/cpp_extension/TtnnGuard.hpp
@@ -0,0 +1,146 @@
+#pragma once
+
+#include <c10/core/DeviceGuard.h>
+#include "extension_utils.hpp"
+#include "ttnn/device.hpp"
+
+// =====================================
+// ============= Device Guards =========
+// =====================================
+
+// PyTorch has an API for registering device guards.
+// Device guards can be used to set the current "active" device,
+// and e.g. error if the user provides an invalid device index.
+//
+// If your device doesn't support indices (e.g. foo:0 vs. foo:1),
+// then the guards probably aren't needed.
+//
+// You can use it by creating a DeviceGuard class, registering it
+// in PyTorch, and invoking the device guard before any kernels are called.
+// For a more full-featured example of a device guard,
+// check out the code at c10/cuda/CUDAGuard.h
+
+// Represents the current "active" device.
+// The device guard registered below is meant to show how a backend
+// can integrate custom device guard with pytorch.
+// For something like cuda this represents the current active cuda device,
+// which is directly set using the cuda API calls cudaGetDevice/cudaSetDevice.
+static uint16_t CURR_DEVICE = -1;
+
+// Create and register a dummy device guard.
+struct TtnnDeviceGuard final : public c10::impl::DeviceGuardImplInterface {
+    static constexpr c10::DeviceType static_type = c10::DeviceType::PrivateUse1;
+    TtnnDeviceGuard() { LOGGING(""); }
+    explicit TtnnDeviceGuard(c10::DeviceType t) {
+        // LOGGING("");
+        TORCH_INTERNAL_ASSERT(t == c10::DeviceType::PrivateUse1);
+    }
+    at::DeviceType type() const override {
+        LOGGING("");
+        return at::DeviceType::PrivateUse1;
+    }
+    at::Device exchangeDevice(at::Device d) const override {
+        // LOGGING("");
+        TORCH_INTERNAL_ASSERT(d.type() == at::DeviceType::PrivateUse1);
+        TORCH_INTERNAL_ASSERT(d.index() < deviceCount(), "Error: device index ", d.index(), " does not exist.");
+        at::Device old_device = getDevice();
+        if (old_device.index() != d.index()) {
+            // "set the active device"
+            CURR_DEVICE = d.index();
+        }
+        return old_device;
+    }
+    at::Device getDevice() const override {
+        // LOGGING("");
+        return at::Device(at::DeviceType::PrivateUse1, CURR_DEVICE);
+    }
+    void setDevice(at::Device d) const override {
+        // LOGGING("");
+        TORCH_INTERNAL_ASSERT(d.type() == at::DeviceType::PrivateUse1);
+        TORCH_INTERNAL_ASSERT(d.index() < deviceCount(), "Error: device index ", d.index(), " does not exist.");
+        at::Device current_device = getDevice();
+        if (current_device != d) {
+            CURR_DEVICE = d.index();
+        }
+    }
+    void uncheckedSetDevice(at::Device d) const noexcept override {
+        // LOGGING("");
+        auto current_device = getDevice();
+        if (current_device != d) {
+            CURR_DEVICE = d.index();
+        }
+    }
+    at::Stream getStream(at::Device d) const noexcept override {
+        // no-op
+        return at::Stream(at::Stream::DEFAULT, d);
+    }
+    // NB: These do NOT set the current device
+    at::Stream exchangeStream(at::Stream) const noexcept override {
+        // no-op
+        return at::Stream(at::Stream::DEFAULT, at::Device(at::DeviceType::PrivateUse1, CURR_DEVICE));
+    }
+    at::DeviceIndex deviceCount() const noexcept override {
+        // Hardcoding the number of "valid" devices here at 2.
+        return 2;
+    }
+
+    // Event-related functions
+    void record(
+        void** /*event*/,
+        const at::Stream& /*stream*/,
+        const at::DeviceIndex /*device_index*/,
+        const c10::EventFlag /*flag*/) const override {
+        TORCH_CHECK(false, at::DeviceType::PrivateUse1, " backend doesn't support events.");
+    }
+    void block(void* /*event*/, const at::Stream& /*stream*/) const override {
+        LOGGING("");
+        TORCH_CHECK(false, at::DeviceType::PrivateUse1, " backend doesn't support events.")
+    }
+    bool queryEvent(void* /*event*/) const override {
+        LOGGING("");
+        TORCH_CHECK(false, at::DeviceType::PrivateUse1, " backend doesn't support events.")
+    }
+    void destroyEvent(void* /*event*/, const at::DeviceIndex /*device_index*/) const noexcept override {}
+
+    // Stream-related functions
+    bool queryStream(const at::Stream& /*stream*/) const override { return true; }
+    void synchronizeStream(const at::Stream& /*stream*/) const override {
+        // Don't wait for anything.
+    }
+};
+
+
+struct TtnnGuard {
+    explicit TtnnGuard() = delete;
+    explicit TtnnGuard(at::DeviceIndex device_index) : guard_(device_index) {}
+    explicit TtnnGuard(at::Device device) : guard_(device) {}
+    TtnnGuard(const TtnnGuard&) = delete;
+    TtnnGuard& operator=(const TtnnGuard&) = delete;
+    TtnnGuard(TtnnGuard&& other) = delete;
+    TtnnGuard& operator=(TtnnGuard&& other) = delete;
+
+    void set_device(at::Device device) { guard_.set_device(device); }
+
+    void reset_device(at::Device device) { guard_.reset_device(device); }
+
+    void set_index(at::DeviceIndex device_index) { guard_.set_index(device_index); }
+
+    at::Device original_device() const { return guard_.original_device(); }
+
+    at::Device current_device() const { return guard_.current_device(); }
+
+    IDevice* get_ttnn_device() {
+        LOGGING("");
+        if (!ttnn_device) {
+            ttnn_device = &ttnn::open_device(0);
+        }
+        return ttnn_device;
+    }
+
+    static IDevice* ttnn_device;
+
+private:
+    c10::impl::InlineDeviceGuard<TtnnDeviceGuard> guard_;
+};
+
+C10_REGISTER_GUARD_IMPL(PrivateUse1, TtnnDeviceGuard);
--- torch_ttnn/cpp_extension/TtnnTensorImpl.cpp
+++ torch_ttnn/cpp_extension/TtnnTensorImpl.cpp
@@ -1,40 +1,22 @@
 #include "TtnnTensorImpl.hpp"
+#include "ttnn/device.hpp"
+#include "ttnn/operations/creation.hpp"
+#include "TtnnGuard.hpp"
 
 namespace at {
 
-// TODO: Only difference is the storage type, combine these two
 TtnnTensorImpl::TtnnTensorImpl(
     at::DispatchKeySet key_set,
     const caffe2::TypeMeta data_type,
     c10::Device device,
-    ttnn::Tensor& ttnn_tensor,
-    c10::intrusive_ptr<c10::StorageImpl> storage) :
-    TensorImpl(key_set, data_type, device),
-    ttnn_tensor_(ttnn_tensor),
-    ttnn_tensor_string_(ttnn_tensor.write_to_string()) {
-    storage_ = std::move(storage);
-    auto view = ttnn_tensor_.get_logical_shape().view();
-    std::vector<int64_t> view_int64;
-    std::copy(view.begin(), view.end(), std::back_inserter(view_int64));
-    IntArrayRef int_array_ref(&(*view_int64.begin()), &(*view_int64.end()));
-    sizes_and_strides_.set_sizes(int_array_ref);
-}
-
-TtnnTensorImpl::TtnnTensorImpl(
-    at::DispatchKeySet key_set,
-    const caffe2::TypeMeta data_type,
-    c10::Device device,
-    const ttnn::Tensor& ttnn_tensor,
+    const at::IntArrayRef& size,
     const c10::Storage& storage) :
-    TensorImpl(key_set, data_type, device),
-    ttnn_tensor_(ttnn_tensor),
-    ttnn_tensor_string_(ttnn_tensor.write_to_string()) {
+    TensorImpl(key_set, data_type, device) {
     storage_ = std::move(storage);
-    auto view = ttnn_tensor_.get_logical_shape().view();
-    std::vector<int64_t> view_int64;
-    std::copy(view.begin(), view.end(), std::back_inserter(view_int64));
-    IntArrayRef int_array_ref(&(*view_int64.begin()), &(*view_int64.end()));
-    sizes_and_strides_.set_sizes(int_array_ref);
+    sizes_and_strides_.set_sizes(size);
+    ttnn::SmallVector<uint32_t> small_vector(size.begin(), size.end());
+    logical_shape_ = ttnn::Shape(small_vector);
+    is_empty_initialized_ = true;
 }
 
 void TtnnTensorImpl::set_sizes_and_strides(const IntArrayRef& int_array_ref) {
@@ -47,14 +29,37 @@ void TtnnTensorImpl::set_sizes_and_strides_as(const at::Tensor& the_template) {
 
 ttnn::Tensor TtnnTensorImpl::get_ttnn_tensor() {
     LOGGING("");
+    if (is_empty_initialized_) {
+        at::Device at_device = GET_OPTIONAL_OR_ASSERT(device_opt_);
+        TtnnGuard device_guard(at_device);
+        IDevice* ttnn_device = device_guard.get_ttnn_device();
+
+        auto ttnn_dtype = dtype_torch_to_ttnn(c10::typeMetaToScalarType(this->dtype()));
+        ttnn_tensor_ = ttnn::empty(
+            get_logical_shape(),
+            ttnn_dtype,
+            ttnn::TILE_LAYOUT,
+            ttnn_device,
+            MemoryConfig{TensorMemoryLayout::INTERLEAVED, BufferType::DRAM, std::nullopt});
+        is_empty_initialized_ = false;
+    }
+
+    TORCH_CHECK(ttnn_tensor_.tensor_attributes != nullptr);
     return ttnn_tensor_;
 }
 
-void TtnnTensorImpl::set_ttnn_tensor(const ttnn::Tensor& tensor) { ttnn_tensor_ = tensor; }
+void TtnnTensorImpl::set_ttnn_tensor(const ttnn::Tensor& tensor) {
+    LOGGING("");
+    // TORCH_CHECK(ttnn_tensor_.tensor_attributes == nullptr);
+    is_empty_initialized_ = false;
+    ttnn_tensor_ = tensor;
+}
 
 c10::intrusive_ptr<TensorImpl> TtnnTensorImpl::shallow_copy_and_detach(
     const c10::VariableVersion& version_counter, bool allow_tensor_metadata_change) const {
-    auto impl = c10::make_intrusive<TtnnTensorImpl>(key_set(), dtype(), device(), ttnn_tensor_, storage_);
+    auto impl = c10::make_intrusive<TtnnTensorImpl>(
+        key_set(), dtype(), device(), sizes_and_strides_.sizes_arrayref(), storage_);
+    impl->set_ttnn_tensor(ttnn_tensor_);
     copy_tensor_metadata(
         /*src_opaque_impl=*/this,
         /*dest_opaque_impl=*/impl.get(),
@@ -66,7 +71,9 @@ c10::intrusive_ptr<TensorImpl> TtnnTensorImpl::shallow_copy_and_detach(
 
 c10::intrusive_ptr<TensorImpl> TtnnTensorImpl::shallow_copy_and_detach(
     c10::VariableVersion&& version_counter, bool allow_tensor_metadata_change) const {
-    auto impl = c10::make_intrusive<TtnnTensorImpl>(key_set(), dtype(), device(), ttnn_tensor_, storage_);
+    auto impl = c10::make_intrusive<TtnnTensorImpl>(
+        key_set(), dtype(), device(), sizes_and_strides_.sizes_arrayref(), storage_);
+    impl->set_ttnn_tensor(ttnn_tensor_);
     copy_tensor_metadata(
         /*src_opaque_impl=*/this,
         /*dest_opaque_impl=*/impl.get(),
@@ -85,6 +92,11 @@ void TtnnTensorImpl::shallow_copy_from(const c10::intrusive_ptr<TensorImpl>& imp
         /*version_counter=*/version_counter(),
         /*allow_tensor_metadata_change=*/allow_tensor_metadata_change());
     refresh_numel();
+}
+
+ttnn::Shape TtnnTensorImpl::get_logical_shape() {
+    // TODO: Do we need to check if this is valid?
+    return logical_shape_;
 }
 
 }  // namespace at
