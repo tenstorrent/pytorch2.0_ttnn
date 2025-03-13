83076917bf51b1dbcc68bcabb10d5587d9dac5af
Kevin Wu <kevinwu@tenstorrent.com>
2025-03-13 00:14:12 +0000

    Clean up functionality test and check for truncation

--- tests/cpp_extension/test_cpp_extension_functionality.py
+++ tests/cpp_extension/test_cpp_extension_functionality.py
@@ -27,7 +27,6 @@ def test_cpp_extension(device, input_shape, dtype):
         torch_tensor = torch_tensor.to(dtype)
     else:
         raise Exception(f"{dtype} not being tested at this time")
-    print(torch_tensor)
 
     logging.info("Transferring to ttnn")
     torch_ttnn_tensor = torch_tensor.to(ttnn_device)
@@ -38,8 +37,6 @@ def test_cpp_extension(device, input_shape, dtype):
     # Compare output of abs op for bfloat16 dtype since ttnn.abs does not support int
     if dtype == torch.bfloat16:
         torch_out = torch.abs(torch_tensor)
-        print(torch_out)
-
         logging.info("Running abs on ttnn")
         ttnn_tensor = ttnn.abs(ttnn_tensor)
     elif dtype == torch.int or dtype == torch.long:
@@ -50,9 +47,9 @@ def test_cpp_extension(device, input_shape, dtype):
     logging.info("calling to_torch")
     ttnn_to_torch = ttnn.to_torch(ttnn_tensor)
     # cast back to dtype because ttnn always return uint32_t for integers
-    ttnn_to_torch = ttnn_to_torch.to(dtype)
-
-    print(ttnn_to_torch)
+    if dtype == torch.long:
+        assert ttnn_to_torch.dtype == torch.int32
+        ttnn_to_torch = ttnn_to_torch.to(dtype)
 
     assert torch.allclose(torch_out, ttnn_to_torch, rtol=0.1, atol=0.1)
 
@@ -65,13 +62,11 @@ def test_empty(device):
 
     logging.info("Creating bfloat tensor from -1 to 1")
     torch_tensor = torch.empty((32, 1, 3, 3), dtype=torch.bfloat16, device=ttnn_device)
-    print(torch_tensor)
 
     logging.info("Get underlying ttnn tensor")
     ttnn_tensor = ttnn_module.get_ttnn_tensor(torch_tensor)
 
     logging.info("calling to_torch")
     ttnn_to_torch = ttnn.to_torch(ttnn_tensor)
-    print(ttnn_to_torch)
 
     # assert torch.allclose(torch_tensor, ttnn_to_torch, rtol=0.1, atol=0.1)
