e37ae8d28643ad30b2e55217bca981ec23bf7452
Joe Malone <jmalone@tenstorrent.com>
2025-03-14 17:09:39 +0000

    Added pytorch support for new avg_pool2d in tt-metal Formatted to_tt_pass with black

--- torch_ttnn/passes/lowering/to_tt_pass.py
+++ torch_ttnn/passes/lowering/to_tt_pass.py
@@ -1056,6 +1056,56 @@ def ReplaceMoreTtManually(gm: torch.fx.GraphModule, use_less_ttnn_op_types: bool
                 # TODO(tt-metal#12099): Currently it doesn't return indices. Pack into tuple to maintain the type
                 return g.call_function(target_wrappers.pack_to_tuple, (output_tensor,))
 
+            if node.target == torch.ops.aten.avg_pool2d.default:
+                params = map_args_to_kwargs(
+                    args,
+                    (
+                        (0, "input_tensor"),
+                        (1, "kernel_size"),
+                        (2, "stride"),
+                        (3, "padding"),
+                        (4, "ceil_mode"),
+                        (5, "count_include_pad"),
+                        (6, "divisor_override"),
+                    ),
+                )
+                input_tensor = params["input_tensor"]
+                input_shape = input_tensor.meta["val"].size()
+                batch_size, in_c, in_h, in_w = input_shape
+                kernel_size = params["kernel_size"]
+                stride = params.get("stride", kernel_size)
+                padding = params.get("padding", (0, 0))
+                ceil_mode = params.get("ceil_mode", False)
+                count_include_pad = params.get("count_include_pad", True)
+                divisor_override = params.get("divisor_override", None)
+
+                if ceil_mode or not count_include_pad or divisor_override is not None:
+                    return None
+                input_tensor = insert_ncx_to_nxc(g, input_tensor)
+                input_tensor = g.call_function(ttnn.reshape, (input_tensor, (1, 1, batch_size * in_h * in_w, in_c)))
+                input_tensor = g.call_function(ttnn.to_layout, (input_tensor, TtnnRowMajorLayout()))
+                output_tensor = g.call_function(
+                    ttnn.avg_pool2d,
+                    (
+                        input_tensor,
+                        batch_size,
+                        in_h,
+                        in_w,
+                        in_c,
+                        kernel_size,
+                        stride,
+                        padding,
+                    ),
+                    (
+                        {
+                            "ceil_mode": ceil_mode,
+                            "count_include_pad": count_include_pad,
+                            "divisor_override": divisor_override,
+                        }
+                    ),
+                )
+                return insert_sharded_nxc_to_ncx(g, output_tensor, node.meta["val"].size())
+
             if node.target == torch.ops.aten._native_batch_norm_legit_no_training.default:
                 return batch_norm_inference(*args)
 
