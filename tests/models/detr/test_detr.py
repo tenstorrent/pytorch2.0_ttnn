8afccd81faa68ff57b1803dfd2e3b507631d8078
Joe Malone <jmalone@tenstorrent.com>
2025-03-17 21:20:30 +0000

    Fixes OOM error in DETR with workaround to layout conv input as row-major
    Fixes #794

--- tests/models/detr/test_detr.py
+++ tests/models/detr/test_detr.py
@@ -38,10 +38,7 @@ class ThisTester(ModelTester):
 @pytest.mark.parametrize(
     "mode",
     [
-        pytest.param(
-            "eval",
-            marks=pytest.mark.compilation_xfail,
-        ),
+        "eval",
     ],
 )
 def test_detr(record_property, mode):
