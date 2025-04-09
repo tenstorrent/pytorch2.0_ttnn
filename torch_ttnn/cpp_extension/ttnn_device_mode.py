8c5c2dbd608ed9b24c4892966d63142f8def00b4
Kevin Wu <kevinwu@tenstorrent.com>
2025-04-09 19:24:04 +0000

    Add workaround problematic compiler macros by undefining them in the commandline

--- torch_ttnn/cpp_extension/ttnn_device_mode.py
+++ torch_ttnn/cpp_extension/ttnn_device_mode.py
@@ -48,11 +48,15 @@ working_directory = working_file_path.parents[0]
 source_file_pattern = Path("*.cpp")
 source_files = glob.glob(str(working_directory / source_file_pattern), recursive=False)
 
+extra_cflags = ["-g", "-DFMT_HEADER_ONLY", "-std=c++20"]
+# Undefine the following problematic macros https://github.com/tenstorrent/tt-metal/issues/20361
+extra_cflags += ["-UPYBIND11_COMPILER_TYPE", "-UPYBIND11_BUILD_ABI"]
+
 ttnn_module = torch.utils.cpp_extension.load(
     name="ttnn_device_extension",
     sources=source_files,
     extra_include_paths=[str(working_directory)] + ttnn_include_paths,
-    extra_cflags=["-g", "-DFMT_HEADER_ONLY", "-std=c++20"],
+    extra_cflags=extra_cflags,
     extra_ldflags=tt_metal_lib_paths + tt_metal_libs,
     verbose=True,
 )
