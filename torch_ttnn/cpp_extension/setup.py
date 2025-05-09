2fa3c84637d8e1c4eaf1adafd74dfaebfe6ff353
Pavlo Hilei <philei@tenstorrent.com>
2025-05-09 16:12:32 +0200

    When using git-submodule, make sure ttnn compiled with same flags as torch

--- torch_ttnn/cpp_extension/setup.py
+++ torch_ttnn/cpp_extension/setup.py
@@ -5,6 +5,32 @@ from setuptools import setup, Extension, find_namespace_packages
 from setuptools.command.build_ext import build_ext
 import sysconfig
 import torch
+from pprint import pprint
+import re
+
+
+def get_torch_abi_related_compiler_flags():
+    """Extract compiler flags relaed to abi from torch.__config__.show()"""
+    config_str = torch.__config__.show()
+
+    # Extract C++ flags
+    cxx_flags = []
+
+    # Look for C++ standard
+    std_match = re.search(r"-std=c\+\+\d+", config_str)
+    if std_match:
+        cxx_flags.append(std_match.group(0))
+
+    # Look for ABI flag, critical for compatibility
+    abi_match = re.search(r"-D_GLIBCXX_USE_CXX11_ABI=(\d)", config_str)
+    if abi_match:
+        cxx_flags.append(f"-D_GLIBCXX_USE_CXX11_ABI={abi_match.group(1)}")
+
+    fabi_match = re.search(r"-fabi-version=(\d+)", config_str)
+    if fabi_match:
+        cxx_flags.append(f"-fabi-version={fabi_match.group(1)}")
+
+    return cxx_flags
 
 
 class CMakeExtension(Extension):
@@ -39,10 +65,20 @@ class CMakeBuild(build_ext):
         extra_cmake_flags = extra_cmake_flags.split(";")
         if "-DENABLE_SUBMODULE_TT_METAL_BUILD=ON" in extra_cmake_flags:
             extra_cmake_flags.append("-DENABLE_LOCAL_TT_METAL_BUILD=OFF")
+            torch_cxx_flags = get_torch_abi_related_compiler_flags()
+            if torch_cxx_flags:
+                flags_str = " ".join(torch_cxx_flags)
+                extra_cmake_flags.append(f'-DCMAKE_CXX_FLAGS="{flags_str}"')
+            # Torch is built with gcc
+            extra_cmake_flags.append("-DCMAKE_CXX_COMPILER=/usr/bin/g++")  # TODO: test different g++ versions
+
+        else:
+            cmake_args.append("-DCMAKE_CXX_COMPILER=clang++-17")
         if extra_cmake_flags:
             cmake_args.extend(extra_cmake_flags)
 
         cmake_args.extend(ext.cmake_args)
+        pprint(cmake_args)
 
         # Build the extension
         subprocess.check_call(["cmake", ext.source_dir] + cmake_args, cwd=build_dir)
@@ -61,7 +97,6 @@ setup(
         CMakeExtension(
             name="ttnn_device_extension",
             source_dir=".",
-            cmake_args=["-DCMAKE_CXX_COMPILER=clang++-17"],
         ),
     ],
     cmdclass={"build_ext": CMakeBuild},
