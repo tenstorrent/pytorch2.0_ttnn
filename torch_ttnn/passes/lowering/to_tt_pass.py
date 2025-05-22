345dfc2bbd6bc33463f7024c3869657c19156985
Diego Gomez <dgomez@tenstorrent.com>
2025-05-22 15:07:41 -0400

    Updated slices validations in the compiler (#877)
    * Updated slide validations, now it can be bigger than 1. Since the compiler already fills with 1 when the array is empty, we can assume that will be always equals or bigger than 1
    
    * pre commit format
    
    * addressed the slicing logic when the step is different than 1
    
    * removed a print that was being used for debugging

--- torch_ttnn/passes/lowering/to_tt_pass.py
+++ torch_ttnn/passes/lowering/to_tt_pass.py
@@ -6,6 +6,7 @@ import ttnn
 import math
 from torch._guards import detect_fake_mode
 from torch._subclasses.fake_tensor import unset_fake_temporarily
+
 from torch_ttnn.utils import (
     GraphCleanup,
     TtnnBfloat16,
@@ -709,11 +710,11 @@ def ReplaceMoreTtManually(gm: torch.fx.GraphModule, device, use_less_ttnn_op_typ
                 [step] = step or [1]
                 rank = len(input_size)
 
-                if step != 1 or dim >= rank:
+                if dim >= rank:
                     return None
 
                 # Check if no-op, just return the input tensor
-                if start == 0 and end >= input_size[dim]:
+                if start == 0 and end >= input_size[dim] and step == 1:
                     return tensor
 
                 slice_start = np.zeros(rank, dtype=int)
@@ -728,7 +729,15 @@ def ReplaceMoreTtManually(gm: torch.fx.GraphModule, device, use_less_ttnn_op_typ
                 slice_start[dim] = start
                 slice_end[dim] = end
 
-                return g.call_function(ttnn.slice, (tensor, [*slice_start], [*slice_end]))
+                if step != 1:
+                    array_step = [1] * len(input_size)
+                    array_step[dim] = step
+
+                kwargs = {
+                    "slice_step": array_step if step != 1 else None,
+                }
+
+                return g.call_function(ttnn.slice, (tensor, [*slice_start], [*slice_end]), kwargs)
 
             if node.target == torch.ops.aten.repeat.default:
                 tensor, sizes = args
@@ -1244,6 +1253,7 @@ def ReplaceMoreTtManually(gm: torch.fx.GraphModule, device, use_less_ttnn_op_typ
                 # slice_scatter could be concat([pre_slice_tensor, src_tensor, post_slice_tensor])
                 rank = len(tensor_shape)
                 [step] = step or [1]
+
                 if step != 1:
                     return None
 
