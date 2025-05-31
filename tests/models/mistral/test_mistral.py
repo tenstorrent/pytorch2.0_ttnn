b6480237ef915eff99d614fb57ee58709f6bc48b
aliraza556 <alirazaakhtar1122@gmail.com>
2025-06-01 02:40:57 +0500

    Add Mistral 7B to model test suite

---
+++ tests/models/mistral/test_mistral.py
@@ -0,0 +1,63 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import torch
+import pytest
+
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from tests.utils import ModelTester
+
+
+class ThisTester(ModelTester):
+    def _load_model(self):
+        # Download model from cloud
+        model_name = "mistralai/Mistral-7B-v0.1"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left", torch_dtype=torch.bfloat16)
+        m = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+        return m
+
+    def _load_inputs(self):
+        # Set up sample input
+        self.test_input = "Tenstorrent hardware accelerates AI models by "
+        inputs = self.tokenizer.encode_plus(
+            self.test_input,
+            return_tensors="pt",
+            max_length=32,
+            padding="max_length",
+            add_special_tokens=True,
+            truncation=True,
+        )
+        return inputs
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [pytest.param("eval", marks=pytest.mark.converted_end_to_end)],
+)
+def test_mistral(record_property, mode):
+    model_name = "Mistral"
+    record_property("model_name", model_name)
+    record_property("mode", mode)
+
+    tester = ThisTester(model_name, mode)
+    results = tester.test_model()
+
+    if mode == "eval":
+        # Helper function to decode output to human-readable text
+        def decode_output(outputs):
+            next_token_logits = outputs.logits[:, -1]
+            next_token = next_token_logits.softmax(dim=-1).argmax()
+            return tester.tokenizer.decode([next_token])
+
+        decoded_output = decode_output(results)
+
+        print(
+            f"""
+        model_name: {model_name}
+        input: {tester.test_input}
+        output: {decoded_output}
+        """
+        )
+
+    record_property("torch_ttnn", (tester, results)) 
