bcca5090fce4e820dbbf4c59dd14f0224afda50b
aliraza556 <alirazaakhtar1122@gmail.com>
2025-06-01 03:20:10 +0500

    Fix black formatting for Mistral 7B test file

--- tests/models/mistral/test_mistral.py
+++ tests/models/mistral/test_mistral.py
@@ -13,7 +13,9 @@ class ThisTester(ModelTester):
     def _load_model(self):
         # Download model from cloud
         model_name = "mistralai/Mistral-7B-v0.1"
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left", torch_dtype=torch.bfloat16)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name, padding_side="left", torch_dtype=torch.bfloat16
+        )
         m = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
         return m
 
@@ -60,4 +62,4 @@ def test_mistral(record_property, mode):
         """
         )
 
-    record_property("torch_ttnn", (tester, results)) 
+    record_property("torch_ttnn", (tester, results))
