b08bb9a4cd0a85955511f2f84dd963aa7073ce6b
Kevin Wu <kevinwu@tenstorrent.com>
2025-08-15 20:49:06 +0000

    TEST COMMIT

--- tools/build_tt_metal.sh
+++ tools/build_tt_metal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e 
+
 while [ $# -gt 0 ]
 do
     case "$1" in
@@ -18,14 +20,14 @@ CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "> Current directory: $CUR_DIR"
 
 # ABI flags used when compiling torch
-if [ -n $TORCH_COMPAT ]; then
-    TORCH_ABI_FLAGS=$(python3 $CUR_DIR/get_torch_abi_flags.py)
-    echo "> TORCH_ABI_FLAGS: $TORCH_ABI_FLAGS"
-    CMAKE_CXX_FLAGS="${TORCH_ABI_FLAGS} -std=c++20"
+# if [ -n $TORCH_COMPAT ]; then
+#     TORCH_ABI_FLAGS=$(python3 $CUR_DIR/get_torch_abi_flags.py)
+#     echo "> TORCH_ABI_FLAGS: $TORCH_ABI_FLAGS"
+#     CMAKE_CXX_FLAGS="${TORCH_ABI_FLAGS} -std=c++20"
 
-    CPP_EXT_DIR="$CUR_DIR/../torch_ttnn/cpp_extension"
-    CMAKE_TOOLCHAIN_FILE=$CPP_EXT_DIR/cmake/x86_64-linux-torch-toolchain.cmake
-fi
+#     CPP_EXT_DIR="$CUR_DIR/../torch_ttnn/cpp_extension"
+#     CMAKE_TOOLCHAIN_FILE=$CPP_EXT_DIR/cmake/x86_64-linux-torch-toolchain.cmake
+# fi
 
 # Configure ttnn
 # TODO: check if c++17 is enough
@@ -50,13 +52,13 @@ cmake -B $CUR_DIR/third-party/tt-metal/build \
 echo "> Building ttnn"
 ninja -C $CUR_DIR/third-party/tt-metal/build install
 
-if [ -n $TORCH_COMPAT ]; then
-    pip3 install -e $CUR_DIR/third-party/tt-metal/
+# if [ -n $TORCH_COMPAT ]; then
+#     pip3 install -e $CUR_DIR/third-party/tt-metal/
 
-    export TT_METAL_HOME=$CUR_DIR/third-party/tt-metal
-    echo "> TT_METAL_HOME: $TT_METAL_HOME"
+#     export TT_METAL_HOME=$CUR_DIR/third-party/tt-metal
+#     echo "> TT_METAL_HOME: $TT_METAL_HOME"
 
-    echo "> Building cpp extension"
-    cd "$CPP_EXT_DIR"
-    CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${BUILD_TYPE};-DCMAKE_C_COMPILER_LAUNCHER=ccache;-DCMAKE_CXX_COMPILER_LAUNCHER=ccache;-DCMAKE_CXX_COMPILER=g++-12;-DCMAKE_C_COMPILER=gcc-12" python3 setup.py develop
-fi
+#     echo "> Building cpp extension"
+#     cd "$CPP_EXT_DIR"
+#     CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${BUILD_TYPE};-DCMAKE_C_COMPILER_LAUNCHER=ccache;-DCMAKE_CXX_COMPILER_LAUNCHER=ccache;-DCMAKE_CXX_COMPILER=g++-12;-DCMAKE_C_COMPILER=gcc-12" python3 setup.py develop
+# fi
