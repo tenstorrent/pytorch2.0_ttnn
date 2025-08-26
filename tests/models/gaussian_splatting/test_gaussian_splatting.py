0c26db7dad55c01018c2e20cae183fc9db39c193
Kevin Wu <kevinwu@tenstorrent.com>
2025-08-26 20:45:56 +0000

    Import gaussian splatting into test directory

--- tests/models/gaussian_splatting/test_gaussian_splatting.py
+++ tests/models/gaussian_splatting/test_gaussian_splatting.py
@@ -12,56 +12,27 @@ import sys
 import torch
 import zipfile
 
+# Install additional python dependencies
+subprocess.check_call([sys.executable, "-m", "pip", "install", "imageio", "accelerate", "einops", "numpy>=1.24.4,<2"])
+
 from tests.utils import ModelTester
 
-# Path to patched gaussian splatting implementation that is compatible with TT
-github_url = "https://github.com/kevinwuTT/torch-splatting/archive/refs/heads/tt-compat.zip"
-# Full path of this Python script
-test_path = os.path.dirname(os.path.realpath(__file__))
-# Path of gaussian splatting module after extraction
-gs_module_path = f"{test_path}/torch-splatting-tt-compat"
-
-
-# Download the zip from github, extract, and load directory as a Python module
-def setup_gs_module():
-    try:
-        response = requests.get(github_url)
-    except Exception as e:
-        print(f"Error downloading URL: {github_url}")
-        raise e
-
-    assert response.status_code == 200
-
-    # Write to disk and extract
-    zip_path = f"{test_path}/tt-compat.zip"
-    open(zip_path, "wb").write(response.content)
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        zip_ref.extractall(path=test_path)
-
-    # Load module into PYTHONPATH
-    sys.path.append(gs_module_path)
-
-    # Extract dataset from reference repo:
-    # https://github.com/hbb1/torch-splatting/blob/main/B075X65R3X.zip
-    # These are images with depth, rgb, and other point information.
-    with zipfile.ZipFile(f"{gs_module_path}/B075X65R3X.zip", "r") as zip_ref:
-        zip_ref.extractall(path=gs_module_path)
-
-    subprocess.check_call(
-        [sys.executable, "-m", "pip", "install", "imageio", "accelerate", "einops", "numpy>=1.24.4,<2"]
-    )
+from gaussian_splatting.gauss_model import GaussModel
+from gaussian_splatting.utils.data_utils import read_all
+from gaussian_splatting.gsstrainer import GSSTrainer
+from gaussian_splatting.utils.point_utils import get_point_clouds
+from gaussian_splatting.gauss_render import GaussRenderer
 
 
 class ThisTester(ModelTester):
     def __init__(self, model_name, mode, batch_size, steps, fp16):
         self.steps = steps
         self.fp16 = fp16
+        self.test_path = os.path.dirname(os.path.realpath(__file__))
         super().__init__(model_name, mode, batch_size)
 
     def _load_model(self):
         # set up model
-        from gaussian_splatting.gauss_model import GaussModel
-
         gaussModel = GaussModel(
             sh_degree=4,
             debug=False,
@@ -70,12 +41,25 @@ class ThisTester(ModelTester):
         return gaussModel
 
     def _load_inputs(self, batch_size):
-        # input data processing
-        from gaussian_splatting.utils.data_utils import read_all
+        # Extract dataset from reference repo:
+        # These are images with depth, rgb, and other point information.
+        dataset_url = "https://github.com/hbb1/torch-splatting/raw/refs/heads/main/B075X65R3X.zip"
+        try:
+            response = requests.get(dataset_url)
+            assert response.status_code == 200
+        except Exception as e:
+            print(f"Error downloading URL: {dataset_url}")
+            raise e
 
+        dataset_path = f"{self.test_path}/B075X65R3X.zip"
+        open(dataset_path, "wb").write(response.content)
+        with zipfile.ZipFile(dataset_path, "r") as zip_ref:
+            zip_ref.extractall(path=self.test_path)
+
+        # input data processing
         dtype = torch.float16 if self.fp16 else torch.float
 
-        folder = f"{gs_module_path}/B075X65R3X"
+        folder = f"{self.test_path}/B075X65R3X"
         data = read_all(folder, resize_factor=0.5)
         data = {k: v.to(dtype) for k, v in data.items()}
         data["depth_range"] = torch.Tensor([[1, 3]] * len(data["rgb"])).to(dtype)
@@ -83,10 +67,6 @@ class ThisTester(ModelTester):
         return data
 
     def setup_gsstrainer(self, as_ttnn=False, option=None):
-        from train import GSSTrainer
-        from gaussian_splatting.utils.point_utils import get_point_clouds
-        from gaussian_splatting.gauss_render import GaussRenderer
-
         gaussModel = self.model
         data = self.inputs
         points = get_point_clouds(data["camera"], data["depth"], data["alpha"], data["rgb"])
@@ -150,8 +130,6 @@ class ThisTester(ModelTester):
 def test_gaussian_splatting(record_property, mode, batch_size, steps, fp16):
     torch.manual_seed(99)
     np.random.seed(99)
-
-    setup_gs_module()
 
     model_name = "GaussianSplatting"
     record_property("model_name", model_name)
