acb08a07a9198e813c7440b5617095912b47e4ea
pollachisaravanan <70937801+pollachisaravanan@users.noreply.github.com>
2025-11-02 09:22:47 +0000

    Support mamba 2.8B pytorch model

---
+++ tests/lowering/eltwise/unary/test_softplus.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import torch
+import torch_ttnn
+import pytest
+import ttnn
+from tests.utils import assert_with_pcc
+
+
+class SoftplusModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input_tensor):
+        return torch.nn.functional.softplus(input_tensor)
+
+
+@pytest.mark.parametrize(
+    "input_shape",
+    [
+        (1, 33, 5120),
+    ],
+)
+def test_softplus(device, input_shape):
+    m = SoftplusModule()
+    input_tensor = torch.rand(input_shape, dtype=torch.bfloat16)
+    result_before = m.forward(input_tensor)
+    option = torch_ttnn.TorchTtnnOption(device=device)
+    option.gen_graphviz = True
+    # The compilation is lazy, so we need to run forward once to trigger the compilation
+    m = torch.compile(m, backend=torch_ttnn.backend, options=option)
+    result_after = m.forward(input_tensor)
+    option._out_fx_graphs[0].print_tabular()
+
+    # Check the graph has be rewritten and contain ttnn ops
+    nodes = list(option._out_fx_graphs[0].nodes)
+    assert [node.target for node in nodes].count(ttnn.softplus) == 1
+
+    # Check inference result
+    assert_with_pcc(result_before, result_after)
